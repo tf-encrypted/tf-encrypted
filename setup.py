b3e851518342926a4ab4da433bebb84ecddd6457
jvmancuso <jason@partydb.io>
2018-08-13 13:55:05 -0400

    generic setuptools file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'TensorflowEncrypted',
+    version = '0.0.1rc',
+    packages = ['tensorflow_encrypted',],
+    license = '',
+    long_description = open('README.md').read(),
+)
