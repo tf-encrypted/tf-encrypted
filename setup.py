1b383343f5e7f9af2d3a32016b6e0bc37b23933b
jvmancuso <jason@partydb.io>
2018-08-13 14:45:44 -0400

    add tf soft dependency

--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ setup(
     name = 'TensorflowEncrypted',
     version = '0.0.1rc',
     packages = ['tensorflow_encrypted',],
+    install_requires = ['numpy>=1.14.0']
+    extra_requires = {
+        'tf': ["tensorflow>=1.0.0"],
+        'tf_gpu': ["tensorflow-gpu>=1.0.0"],
+    }
     license = '',
     long_description = open('README.md').read(),
 )
