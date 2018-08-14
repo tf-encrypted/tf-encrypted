fd7210d621b9037d0476cec3ae3da5b485dd9196
Ben DeCoste <bendecoste@gmail.com>
2018-08-14 15:13:25 -0300

    fix lint errors in protocol/__init__.py

--- tensorflow_encrypted/protocol/__init__.py
+++ tensorflow_encrypted/protocol/__init__.py
@@ -1,30 +1,37 @@
+from typing import Optional, Any, Type
+from types import TracebackType
 from __future__ import absolute_import
-
-_current_prot = None
-
-def set_protocol(prot):
-    global _current_prot
-    _current_prot = prot
-
-def get_protocol():
-    return _current_prot
-
-class Player(object):
-    
-    def __init__(self, device_name):
-        self.device_name = device_name
-
-class Protocol(object):
-
-    def __enter__(self):
-        set_protocol(self)
-        return self
-
-    def __exit__(self, type, value, traceback):
-        set_protocol(None)
 
 # from unencrypted_native import UnencryptedNative
 # from unencrypted_fixedpoint import UnencryptedFixedpoint
 from .pond import Pond
 from .securenn import SecureNN
 # from secureml import SecureML
+
+
+_current_prot = None
+
+
+def set_protocol(prot: Optional[Protocol]) -> None:
+    global _current_prot
+    _current_prot = prot
+
+
+def get_protocol() -> Optional[Protocol]:
+    return _current_prot
+
+
+class Player(object):
+    def __init__(self, device_name: str) -> None:
+        self.device_name = device_name
+
+
+class Protocol(object):
+    def __enter__(self) -> Protocol:
+        set_protocol(self)
+        return self
+
+    def __exit__(self, type: Optional[Type[BaseException]],
+                 value: Optional[Exception],
+                 traceback: Optional[TracebackType]) -> Optional[None]:
+        set_protocol(None)
