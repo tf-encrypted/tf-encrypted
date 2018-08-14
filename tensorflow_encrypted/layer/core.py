53ca8d06a0078b06a4133f84981e63d23a475404
jvmancuso <jason@partydb.io>
2018-08-14 15:11:17 -0400

    Layer now an ABC

--- tensorflow_encrypted/layer/core.py
+++ tensorflow_encrypted/layer/core.py
@@ -1,12 +1,24 @@
 import numpy as np
 import math
+from abc import ABC, abstractmethod
 from ..protocol import get_protocol
 
 # TODO
 # split backward function in compute_gradient and compute_backpropagated_error?
 
 
-class Layer(object):
+class Layer(ABC):
+    @abstractmethod
+    def initialize(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def forward(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def backward(self, *args, **kwargs):
+        pass
 
     @property
     def prot(self):
