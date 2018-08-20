ad1e7582c4b6959fce624317eab2e565c3bf96e2
yann dupis <yann.dupis@gmail.com>
2018-08-20 15:14:58 -0700

    Add node support for Ass

--- tests/convert_test.py
+++ tests/convert_test.py
@@ -105,6 +105,55 @@ class TestConvert(unittest.TestCase):
 
         np.testing.assert_array_almost_equal(output, actual, decimal=3)
 
+
+    def test_add_convert(self):
+        tf.reset_default_graph()
+
+        filename = "add.pb"
+
+        input_shape = [28, 1]
+
+        path = export_add(filename, input_shape)
+
+        tf.reset_default_graph()
+
+        graph_def = read_graph(path)
+
+        tf.reset_default_graph()
+
+        actual = run_add(input_shape)
+
+        tf.reset_default_graph()
+
+        config = tfe.LocalConfig([
+            'server0',
+            'server1',
+            'crypto_producer',
+            'prediction_client',
+            'weights_provider'
+        ])
+
+        with tfe.protocol.Pond(*config.get_players('server0, server1, crypto_producer')) as prot:
+            prot.clear_initializers()
+
+            class PredictionClient(tfe.io.InputProvider):
+                def provide_input(self):
+                    return tf.constant(np.ones(input_shape))
+
+            input = PredictionClient(config.get_player('prediction_client'))
+
+            converter = Converter(config, prot, config.get_player('weights_provider'))
+
+            x = converter.convert(graph_def, input, register())
+
+            with config.session() as sess:
+                tfe.run(sess, prot.initializer, tag='init')
+
+                output = x.reveal().eval(sess, tag='reveal')
+
+        np.testing.assert_array_almost_equal(output, actual, decimal=3)
+
+
     def test_strided_slice_convert(self):
         tf.reset_default_graph()
 
@@ -156,46 +205,6 @@ class TestConvert(unittest.TestCase):
 
         np.testing.assert_array_almost_equal(output, actual, decimal=3)
 
-    def test_strided_slice_convert(self):
-        tf.reset_default_graph()
-
-        filename = "strided_slice.pb"
-
-        path = export_strided_slice(filename)
-
-        tf.reset_default_graph()
-
-        graph_def = read_graph(path)
-
-        tf.reset_default_graph()
-
-        input = [[[1, 1, 1], [2, 2, 2]],
-                 [[3, 3, 3], [4, 4, 4]],
-                 [[5, 5, 5], [6, 6, 6]]]
-
-        actual = run_strided_slice(input)
-
-        tf.reset_default_graph()
-
-        config = tfe.LocalConfig([
-            'server0',
-            'server1',
-            'crypto_producer'
-        ])
-
-        with tfe.protocol.Pond(*config.get_players('server0, server1, crypto_producer')) as prot:
-            prot.clear_initializers()
-            input = prot.define_private_variable(np.array(input))
-
-            x = convert(graph_def, input)
-
-            with config.session() as sess:
-                tfe.run(sess, prot.initializer, tag='init')
-
-                output = x.reveal().eval(sess, tag='reveal')
-
-        np.testing.assert_array_almost_equal(output, actual, decimal=3)
-
 
 def run_cnn(input_shape: List[int]):
     input = tf.placeholder(tf.float32, shape=input_shape, name="input")
@@ -239,6 +248,26 @@ def export_matmul(filename: str, input_shape: List[int]):
     b = tf.constant(np.ones((input_shape[1], 1)), dtype=tf.float32)
 
     x = tf.matmul(a, b)
+
+    return export(x, filename)
+
+def run_add(input_shape: List[int]):
+    a = tf.placeholder(tf.float32, shape=input_shape, name="input")
+    b = tf.constant(np.ones((input_shape[1], 1)), dtype=tf.float32)
+
+    x = tf.add(a, b)
+
+    with tf.Session() as sess:
+        output = sess.run(x, feed_dict={a: np.ones(input_shape)})
+
+    return output
+
+
+def export_add(filename: str, input_shape: List[int]):
+    a = tf.placeholder(tf.float32, shape=input_shape, name="input")
+    b = tf.constant(np.ones((input_shape[1], 1)), dtype=tf.float32)
+
+    x = tf.add(a, b)
 
     return export(x, filename)
 
