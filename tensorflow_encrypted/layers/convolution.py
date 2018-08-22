b6d5a208259a1435b228d45e9b00f159e69c7724
Justin Patriquin <justin@sidestory.email>
2018-08-22 12:28:05 -0300

    raise exception

--- tensorflow_encrypted/layers/convolution.py
+++ tensorflow_encrypted/layers/convolution.py
@@ -72,7 +72,8 @@ class Conv2D(core.Layer):
         return out
 
     def backward(self, d_y, learning_rate):
-        assert self.channels_first
+        if not channels_first:
+            raise TypeError("channels must be first on the backward pass")
 
         x = self.cache
         h_filter, w_filter, d_filter, n_filter = map(int, self.weights.shape)
