Fix valid average pooling

tests/test_pooling.py
@@ -24,7 +24,7 @@ class TestAveragePooling2D(unittest.TestCase):
         input_shape = (batch_size, channels_in, img_height, img_width)
 
         n_elements = batch_size * channels_in * img_height * img_width
-        input_pool = np.arange(n_elements, dtype=np.float32).reshape(input_shape)
+        input_pool = np.ones(n_elements, dtype=np.float32).reshape(input_shape)
 
         config = tfe.LocalConfig([
             'server0',
@@ -35,15 +35,16 @@ class TestAveragePooling2D(unittest.TestCase):
         return input_pool, input_shape, config
 
     def _tf_tiled_forward(self, input_pool: np.ndarray) -> np.ndarray:
-        with tf.Session() as sess:
-            x = tf.constant(input_pool, dtype=tf.float32)
-            x_NHWC = tf.transpose(x, (0, 2, 3, 1))
-            ksize = [1, 2, 2, 1]
-            pool_out_tf = tf.nn.avg_pool(x_NHWC, ksize=ksize, strides=ksize, padding="SAME")
+        x = tf.constant(input_pool, dtype=tf.float32)
+        x_NHWC = tf.transpose(x, (0, 2, 3, 1))
+        ksize = [1, 2, 2, 1]
+        pool_out_tf = tf.nn.avg_pool(
+            x_NHWC, ksize=ksize, strides=ksize, padding="VALID", data_format='NHWC')
 
+        with tf.Session() as sess:
             out_tf = sess.run(pool_out_tf).transpose(0, 3, 1, 2)
 
-            return out_tf
+        return out_tf
 
     def _generic_tiled_forward(self, t_type: str, even: bool = True) -> None:
         assert t_type in ['public', 'private', 'masked']
@@ -57,7 +58,7 @@ class TestAveragePooling2D(unittest.TestCase):
                 x_in = prot.define_private_variable(input_pool)
             if t_type == 'masked':
                 x_in = prot.mask(x_in)
-            pool = AveragePooling2D(list(input_shape), pool_size=2)
+            pool = AveragePooling2D(list(input_shape), pool_size=2, padding="VALID")
             pool_out_pond = pool.forward(x_in)
 
             with config.session() as sess:
@@ -75,17 +76,23 @@ class TestAveragePooling2D(unittest.TestCase):
 
         np.testing.assert_array_almost_equal(out_pond, out_tf, decimal=3)
 
-    # def test_public_tiled_forward(self) -> None:
-    #     self._generic_tiled_forward('public', True)
+    def test_public_tiled_forward(self) -> None:
+        self._generic_tiled_forward('public', True)
 
     def test_public_forward(self) -> None:
         self._generic_tiled_forward('public', False)
 
-    # def test_private_tiled_forward(self) -> None:
-    #     self._generic_tiled_forward('private')
+    def test_private_tiled_forward(self) -> None:
+        self._generic_tiled_forward('private')
 
-    # def test_masked_tiled_forward(self) -> None:
-    #     self._generic_tiled_forward('masked')
+    def test_private_forward(self) -> None:
+        self._generic_tiled_forward('private', False)
+
+    def test_masked_tiled_forward(self) -> None:
+        self._generic_tiled_forward('masked')
+
+    def test_masked_forward(self) -> None:
+        self._generic_tiled_forward('masked', False)
 
 
 if __name__ == '__main__':