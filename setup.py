c3f218c70209eb7a2dec3eb9c8b89ea47ec2a71f
Morten Dahl <mortendahlcs@gmail.com>
2018-09-03 16:03:52 +0200

    ..

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='tf-encrypted',
     version='0.0.1rc',
     packages=['tensorflow_encrypted',],
-    python_requires='==3.6',
+    python_requires='>=3.6',
     install_requires=[
         'tensorflow>=1.10.0',
         'numpy>=1.14.0',
@@ -15,5 +15,5 @@ setup(
     },
     license='Apache License 2.0',
     url="https://github.com/mortendahl/tf-encrypted",
-    long_description='Layer on top of TensorFlow for doing machine learning on encrypted data.',
+    description='Layer on top of TensorFlow for doing machine learning on encrypted data.',
 )
