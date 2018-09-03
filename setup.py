91b92c140c5d02075413709d34ad87c1e07f7faf
Morten Dahl <mortendahlcs@gmail.com>
2018-09-03 15:37:55 +0200

    ..

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 
 setup(
-    name = 'tf-encrypted',
-    version = '0.0.1rc',
-    packages = ['tensorflow_encrypted',],
-    install_requires = ['numpy>=1.14.0'],
-    extra_requires = {
+    name='tf-encrypted',
+    version='0.0.1rc',
+    packages=['tensorflow_encrypted',],
+    install_requires=[
+        'tensorflow>=1.10.0',
+        'numpy>=1.14.0',
+    ],
+    extra_requires={
         'tf': ["tensorflow>=1.0.0"],
         'tf_gpu': ["tensorflow-gpu>=1.0.0"],
     },
-    license = '',
-    long_description = '',
+    license='Apache License 2.0',
+    url="https://github.com/mortendahl/tf-encrypted",
+    long_description='Layer on top of TensorFlow for doing machine learning on encrypted data.',
 )
