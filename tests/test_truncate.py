04be103691902f40abdd207a480d484aa7267e5e
Morten Dahl <mortendahlcs@gmail.com>
2018-09-10 22:31:34 +0200

    ..

--- tests/test_truncate.py
+++ tests/test_truncate.py
@@ -5,11 +5,11 @@ import tensorflow as tf
 import tensorflow_encrypted as tfe
 
 
-class TestReshape(unittest.TestCase):
+class TestTruncate(unittest.TestCase):
     def setUp(self):
         tf.reset_default_graph()
 
-    def test_truncate(self):
+    def test_interactive_truncate(self):
 
         config = tfe.LocalConfig([
             'server0',
@@ -22,6 +22,33 @@ class TestReshape(unittest.TestCase):
         prot = tfe.protocol.Pond(
             server0, server1, cp,
             use_interactive_truncation=True
+        )
+
+        with config.session() as sess:
+
+            expected = np.array([12345.6789])
+
+            w = prot.define_private_variable(expected * (2**16))  # double precision
+            v = prot.truncate(w)  # single precision
+
+            sess.run(tf.global_variables_initializer())
+            actual = v.reveal().eval(sess, tag='foo')
+
+            assert np.isclose(actual, expected).all(), actual
+
+    def test_noninteractive_truncate(self):
+
+        config = tfe.LocalConfig([
+            'server0',
+            'server1',
+            'crypto_producer'
+        ])
+
+        server0, server1, cp = config.get_players('server0, server1, crypto_producer')
+
+        prot = tfe.protocol.Pond(
+            server0, server1, cp,
+            use_interactive_truncation=False
         )
 
         with config.session() as sess:
