fixes crt overflow error by splitting matmul in smaller matmuls in th… (#95)
* fixes crt overflow error by splitting matmul in smaller matmuls in the center

* fix line breaks

tensorflow_encrypted/protocol/pond.py
@@ -201,7 +201,8 @@ class Pond(Protocol):
                     return [helper(v) for v in inputs]
 
                 else:
-                    raise TypeError("Don't know how to handle inputs of type {}".format(type(inputs)))
+                    raise TypeError(
+                        "Don't know how to handle inputs of type {}".format(type(inputs)))
 
     def define_private_input(
         self,
@@ -212,7 +213,8 @@ class Pond(Protocol):
     ) -> Union['PondPrivateTensor', 'PondMaskedTensor', List['PondPrivateTensor'], List['PondMaskedTensor']]:
 
         def helper(v: tf.Tensor):
-            assert v.shape.is_fully_defined(), "Shape of input '{}' on '{}' is not fully defined".format(name if name else '', provider.player.name)
+            assert v.shape.is_fully_defined(), "Shape of input '{}' on '{}' is not fully defined".format(
+                name if name else '', provider.player.name)
 
             v = _encode(v, apply_scaling)
             x0, x1 = _share(v)
@@ -243,7 +245,8 @@ class Pond(Protocol):
                     output = [helper(v) for v in inputs]
 
                 else:
-                    raise TypeError("Don't know how to handle inputs of type {}".format(type(inputs)))
+                    raise TypeError(
+                        "Don't know how to handle inputs of type {}".format(type(inputs)))
 
         return output
 
@@ -1164,7 +1167,8 @@ class PondCachedMaskedTensor(PondMaskedTensor):
         assert isinstance(alpha_on_1, BackingTensor), type(alpha_on_1)
         assert isinstance(updator, tf.Operation), type(updator)
 
-        super(PondCachedMaskedTensor, self).__init__(prot, unmasked, a, a0, a1, alpha_on_0, alpha_on_1)
+        super(PondCachedMaskedTensor, self).__init__(
+            prot, unmasked, a, a0, a1, alpha_on_0, alpha_on_1)
         self.updator = updator
 
     def __repr__(self):
@@ -2072,7 +2076,23 @@ def _dot_masked_masked(prot, x, y):
 
 
 def _conv2d_public_public(prot, x, y, strides, padding):
-    raise NotImplementedError()
+    assert isinstance(x, PondPublicTensor), type(x)
+    assert isinstance(y, PondPublicTensor), type(y)
+
+    x_0, x_1 = x.unwrapped
+    y_0, y_1 = y.unwrapped
+
+    with tf.name_scope('conv2d'):
+
+        with tf.device(prot.server_0.device_name):
+            z0 = x_0.conv2d(y_0, strides, padding)
+
+        with tf.device(prot.server_1.device_name):
+            z1 = x_1.conv2d(y_1, strides, padding)
+
+    z = PondPublicTensor(prot, z0, z1)
+    z = prot.truncate(z)
+    return z
 
 
 def _conv2d_public_private(prot, x, y, strides, padding):

tensorflow_encrypted/tensor/int100.py
@@ -9,7 +9,7 @@ from .crt import (
     gen_crt_decompose, gen_crt_recombine_lagrange, gen_crt_recombine_explicit,
     gen_crt_add, gen_crt_sub, gen_crt_mul, gen_crt_dot, gen_crt_mod,
     gen_crt_sum, gen_crt_im2col,
-    gen_crt_sample_uniform, gen_crt_sample_bounded
+    gen_crt_sample_uniform, gen_crt_sample_bounded, crt_matmul_split
 )
 from .helpers import prod, log2
 from ..config import run
@@ -32,6 +32,8 @@ M = prod(m)
 for mi in m:
     assert 2 * log2(mi) + log2(1024) < log2(INT_TYPE.max)
 
+DOT_THRESHOLD = 1024
+
 _crt_decompose = gen_crt_decompose(m)
 _crt_recombine_lagrange = gen_crt_recombine_lagrange(m)
 _crt_recombine_explicit = gen_crt_recombine_explicit(m, INT_TYPE)
@@ -52,6 +54,7 @@ class Int100Tensor(object):
 
     modulus = M
     int_type = INT_TYPE
+    backing: Union[List[np.ndarray], List[tf.Tensor]]
 
     def __init__(
         self,
@@ -142,7 +145,7 @@ class Int100Tensor(object):
         return _squeeze(self, axis)
 
 
-def _lift(x):
+def _lift(x: Union[Int100Tensor, int]) -> Int100Tensor:
     # TODO[Morten] support other types of `x`
 
     if isinstance(x, Int100Tensor):
@@ -177,9 +180,20 @@ def _mul(x, y):
     return Int100Tensor.from_decomposed(z_backing)
 
 
-def _dot(x, y):
+def _dot(x: Union[Int100Tensor, int], y: Union[Int100Tensor, int]) -> Int100Tensor:
     x, y = _lift(x), _lift(y)
-    z_backing = _crt_dot(x.backing, y.backing)
+
+    if x.shape[1] > DOT_THRESHOLD:
+        split_backing = crt_matmul_split(x.backing, y.backing, DOT_THRESHOLD)
+
+        backings = [_crt_dot(xi, yi) for xi, yi in split_backing]
+
+        z_backing = backings[0]
+        for i in range(1, len(backings)):
+            z_backing = _crt_add(z_backing, backings[i])
+    else:
+        z_backing = _crt_dot(x.backing, y.backing)
+
     return Int100Tensor.from_decomposed(z_backing)
 
 
@@ -257,15 +271,21 @@ def _squeeze(x, axis=None):
     return Int100Tensor.from_decomposed(backing)
 
 
-def stack(x: List[Int100Tensor], axis: int = 0):
-    assert all([isinstance(i, Int100Tensor) for i in x])
+def stack(xs: List[Int100Tensor], axis: int = 0):
+    assert all(isinstance(x, Int100Tensor) for x in xs)
+    backing = [
+        tf.stack([x.backing[i] for x in xs], axis=axis)
+        for i in range(len(xs[0].backing))
+    ]
+    return Int100Tensor.from_decomposed(backing)
 
-    backing = []
-    for i in range(len(x[0].backing)):
-        stacked = [j.backing[i] for j in x]
 
-        backing.append(tf.stack(stacked, axis=axis))
-
+def concat(xs: List[Int100Tensor], axis: int = 0):
+    assert all(isinstance(x, Int100Tensor) for x in xs)
+    backing = [
+        tf.concat([x.backing[i] for x in xs], axis=axis)
+        for i in range(len(xs[0].backing))
+    ]
     return Int100Tensor.from_decomposed(backing)
 
 
@@ -335,7 +355,8 @@ class Int100Variable(Int100Tensor):
 
         assert type(int100_initial_value) in [Int100Tensor], type(int100_initial_value)
 
-        variables = [tf.Variable(vi, dtype=Int100Tensor.int_type, trainable=False) for vi in int100_initial_value.backing]
+        variables = [tf.Variable(vi, dtype=Int100Tensor.int_type, trainable=False)
+                     for vi in int100_initial_value.backing]
         backing = [vi.read_value() for vi in variables]
 
         super(Int100Variable, self).__init__(None, backing)