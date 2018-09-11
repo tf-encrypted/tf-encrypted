3798e0894582cae2d8aed5c17e87e2451419d80b
Morten Dahl <mortendahlcs@gmail.com>
2018-09-11 14:46:50 +0200

    ..

---
+++ examples/mnist/serve.py
@@ -0,0 +1,158 @@
+import sys
+from typing import List
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_encrypted as tfe
+
+from convert import decode
+
+if len(sys.argv) > 1:
+    # config file was specified
+    config_file = sys.argv[1]
+    config = tfe.config.load(config_file)
+else:
+    # default to using local config
+    config = tfe.LocalConfig([
+        'server0',
+        'server1',
+        'crypto-producer',
+        'model-trainer',
+        'prediction-client'
+    ])
+
+
+class ModelTrainer(tfe.io.InputProvider):
+
+    BATCH_SIZE = 30
+    ITERATIONS = 60000 // BATCH_SIZE
+    EPOCHS = 1
+
+    def build_data_pipeline(self):
+
+        def normalize(image, label):
+            image = tf.cast(image, tf.float32) / 255.0
+            return image, label
+
+        dataset = tf.data.TFRecordDataset(["./data/train.tfrecord"])
+        dataset = dataset.map(decode)
+        dataset = dataset.map(normalize)
+        dataset = dataset.repeat()
+        dataset = dataset.batch(self.BATCH_SIZE)
+
+        iterator = dataset.make_one_shot_iterator()
+        return iterator
+
+    def build_training_graph(self, training_data) -> List[tf.Tensor]:
+
+        # model parameters and initial values
+        w0 = tf.Variable(tf.random_normal([28 * 28, 512]))
+        b0 = tf.Variable(tf.zeros([512]))
+        w1 = tf.Variable(tf.random_normal([512, 10]))
+        b1 = tf.Variable(tf.zeros([10]))
+
+        # optimizer and data pipeline
+        optimizer = tf.train.AdamOptimizer(learning_rate=0.01)
+
+        # training loop
+        def loop_body(i):
+
+            # get next batch
+            x, y = training_data.get_next()
+
+            # model construction
+            layer0 = tf.matmul(x, w0) + b0
+            layer1 = tf.nn.sigmoid(layer0)
+            layer2 = tf.matmul(layer1, w1) + b1
+
+            predictions = layer2
+            loss = tf.reduce_mean(tf.losses.sparse_softmax_cross_entropy(logits=predictions, labels=y))
+            with tf.control_dependencies([optimizer.minimize(loss)]):
+                return i + 1
+
+        loop = tf.while_loop(lambda i: i < self.ITERATIONS * self.EPOCHS, loop_body, (0,))
+
+        # return model parameters after training
+        loop = tf.Print(loop, [], message="Training complete")
+        with tf.control_dependencies([loop]):
+            return [w0.read_value(), b0.read_value(), w1.read_value(), b1.read_value()]
+
+    def provide_input(self) -> List[tf.Tensor]:
+        with tf.name_scope('loading'):
+            training_data = self.build_data_pipeline()
+
+        with tf.name_scope('training'):
+            parameters = self.build_training_graph(training_data)
+
+        return parameters
+
+
+class PredictionClient(tfe.io.InputProvider, tfe.io.OutputReceiver):
+
+    def provide_input(self) -> tf.Tensor:
+        prediction_input_queue = tf.FIFOQueue(capacity=10, dtypes=[tf.float32], shapes=[1, 28 * 28], shared_name='prediction_input_queue')
+        prediction_input = prediction_input_queue.dequeue()
+        return prediction_input
+
+    def receive_output(self, likelihoods: tf.Tensor) -> tf.Operation:
+        prediction = tf.argmax(likelihoods, axis=1)
+        op = tf.Print([], [prediction], summarize=10, message="ACTUAL ")
+        return op
+
+
+model_trainer = ModelTrainer(config.get_player('model-trainer'))
+prediction_client = PredictionClient(config.get_player('prediction-client'))
+
+server0 = config.get_player('server0')
+server1 = config.get_player('server1')
+crypto_producer = config.get_player('crypto-producer')
+
+with tfe.protocol.Pond(server0, server1, crypto_producer) as prot:
+
+    # get model parameters as private tensors from model owner
+    w0, b0, w1, b1 = prot.define_private_input(model_trainer, masked=True)  # pylint: disable=E0632
+
+    # we'll use the same parameters for each prediction so we cache them to avoid re-training each time
+    w0, b0, w1, b1 = prot.cache([w0, b0, w1, b1])
+
+    # get prediction input from client
+    x = prot.define_private_input(prediction_client, masked=True)  # pylint: disable=E0632
+
+    # compute prediction
+    layer0 = prot.dot(x, w0) + b0
+    layer1 = prot.sigmoid(layer0 * 0.1)  # input normalized to avoid large values
+    layer2 = prot.dot(layer1, w1) + b1
+    prediction = layer2
+
+    # send prediction output back to client
+    prediction_op = prot.define_output(prediction, prediction_client)
+
+
+target = sys.argv[2] if len(sys.argv) > 2 else None
+with config.session(target) as sess:
+
+    print("Init")
+    tfe.run(sess, tf.global_variables_initializer(), tag='init')
+
+    print("Training")
+    tfe.run(sess, tfe.global_caches_updator(), tag='training')
+
+    print("Enqueue prediction")
+    prediction_input_queue = tf.FIFOQueue(capacity=10, dtypes=[tf.float32], shared_name='prediction_input_queue')
+    prediction_input = tf.placeholder(dtype=tf.float32)
+    enqueue_op = prediction_input_queue.enqueue(prediction_input)
+    sess.run(enqueue_op, feed_dict={prediction_input: np.zeros([28*28])})
+
+    for _ in range(5):
+        print("Predicting")
+        tfe.run(sess, prediction_op, tag='prediction')
+
+
+# with config.session(target) as sess:
+
+#     prediction_input_queue = tf.FIFOQueue(10, [28 * 28], shared_name='prediction_input_queue')
+#     prediction_input = tf.placeholder(dtype=tf.float32)
+#     enqueue_op = prediction_input_queue.enqueue(prediction_input)
+
+#     print("Enqueue")
+#     sess.run(enqueue_op, feed_dict={prediction_input: np.zeros([28*28])})
