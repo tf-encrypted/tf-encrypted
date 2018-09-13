ad44ffadb7f67a38a79502bc2f206daa368c7a28
jvmancuso <jason@partydb.io>
2018-09-13 13:30:40 -0400

    wipe out stale securenn code

--- tensorflow_encrypted/protocol/securenn.py
+++ tensorflow_encrypted/protocol/securenn.py
@@ -1,5 +1,4 @@
 from ..protocol import Pond
-from ..protocol.pond import _nodes, _lift, _type
 
 
 class SecureNN(Pond):
@@ -7,83 +6,29 @@ class SecureNN(Pond):
     def select_share(self, x, y):
         raise NotImplementedError
 
-    def private_compare(self, x, y):
+    def bitwise_xor(self, x, y):
         raise NotImplementedError
 
-    def share_convert(self, x, y):
+    def private_compare(self, x, r, beta):
         raise NotImplementedError
 
-    def compute_msb(self, x, y):
+    def share_convert(self, x):
+        raise NotImplementedError
+
+    def compute_msb(self, x):
+        raise NotImplementedError
+
+    def divide(self, x, y):
         raise NotImplementedError
 
     def drelu(self, x):
-        node_key = ('dReLU', x)
-        z = _nodes.get(node_key, None)
-
-        if z is not None:
-            return z
-
-        x = _lift(self, x)
-        dispatch = {}
-        func = dispatch.get(_type(x), None)
-        if func is None:
-            raise TypeError("Don't know how to dReLU {}".format(type(x)))
-
-        z = func(self, x)
-        _nodes[node_key] = z
-
-        return z
+        raise NotImplementedError
 
     def relu(self, x):
-        node_key = ('ReLU', x)
-        z = _nodes.get(node_key, None)
-
-        if z is not None:
-            return z
-
-        x = _lift(self, x)
-        dispatch = {}
-        func = dispatch.get(_type(x), None)
-        if func is None:
-            raise TypeError("Don't know how to ReLU {}".format(type(x)))
-
-        z = func(self, x)
-        _nodes[node_key] = z
-
-        return z
+        raise NotImplementedError
 
     def max_pool(self, x):
-        node_key = ('max_pool', x)
-        z = _nodes.get(node_key, None)
-
-        if z is not None:
-            return z
-
-        x = _lift(self, x)
-        dispatch = {}
-        func = dispatch.get(_type(x), None)
-        if func is None:
-            raise TypeError("Don't know how to max_pool {}".format(type(x)))
-
-        z = func(self, x)
-        _nodes[node_key] = z
-
-        return z
+        raise NotImplementedError
 
     def dmax_pool_efficient(self, x):
-        node_key = ('dmax_pool', x)
-        z = _nodes.get(node_key, None)
-
-        if z is not None:
-            return z
-
-        x = _lift(self, x)
-        dispatch = {}
-        func = dispatch.get(_type(x), None)
-        if func is None:
-            raise TypeError("Don't know how to dmax_pool {}".format(type(x)))
-
-        z = func(self, x)
-        _nodes[node_key] = z
-
-        return z
+        raise NotImplementedError
