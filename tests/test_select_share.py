a0c5b4cd9d00603df7cd54c961f27f629215ef08
Ben DeCoste <bendecoste@gmail.com>
2018-09-20 14:12:56 -0300

    Implement select share (secureNN) (#154)
    Implements select share in the secureNN protocol and adds a test

---
+++ tests/test_select_share.py
@@ -0,0 +1,39 @@
+import unittest
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_encrypted as tfe
+
+
+class TestSelectShare(unittest.TestCase):
+
+    def test_selectShare(self):
+
+        config = tfe.LocalConfig([
+            'server0',
+            'server1',
+            'crypto_producer'
+        ])
+
+        alice = np.array([1, 1, 1, 1]).astype(np.float32)
+        bob = np.array([2, 2, 2, 2]).astype(np.float32)
+        bit = np.array([1, 0, 1, 0]).astype(np.float32)
+
+        expected = np.array([2, 1, 2, 1]).astype(np.float32)
+
+        with tfe.protocol.SecureNN(*config.get_players('server0, server1, crypto_producer')) as prot:
+            alice_input = prot.define_private_variable(alice)
+            bob_input = prot.define_private_variable(bob)
+            bit_input = prot.define_private_variable(bit)
+
+            select = prot.select_share(alice_input, bob_input, bit_input)
+
+            with config.session() as sess:
+                sess.run(tf.global_variables_initializer())
+                chosen = select.reveal().eval(sess)
+
+                assert(np.array_equal(expected, chosen))
+
+
+if __name__ == '__main__':
+    unittest.main()
