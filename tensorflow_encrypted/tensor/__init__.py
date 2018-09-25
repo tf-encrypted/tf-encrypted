a1a92490b274752aa39e63bf29e22b9e614693d7
Justin Patriquin <justinpatriquin@gmail.com>
2018-09-25 09:36:25 -0300

    int64 plus other improvements for shared functions (#172)
    This PR adds a new int64 tensor and moves the binarize function to a shared module. Other shared functions such as conv2d can be added here.

--- tensorflow_encrypted/tensor/__init__.py
+++ tensorflow_encrypted/tensor/__init__.py
@@ -14,6 +14,24 @@ from .prime import (
     PrimeConstant
 )
 
+from .int32 import (
+    Int32Tensor,
+    Int32Placeholder,
+    Int32Variable,
+    Int32Constant
+)
+
+from .int64 import (
+    Int64Tensor,
+    Int64Placeholder,
+    Int64Variable,
+    Int64Constant
+)
+
+from .native_shared import (
+    binarize,
+)
+
 __all__ = [
     'Int100Constant',
     'Int100Placeholder',
@@ -22,5 +40,14 @@ __all__ = [
     'PrimeTensor',
     'PrimePlaceholder',
     'PrimeVariable',
-    'PrimeConstant'
+    'PrimeConstant',
+    'Int32Tensor',
+    'Int32Placeholder',
+    'Int32Variable',
+    'Int32Constant',
+    'Int64Tensor',
+    'Int64Placeholder',
+    'Int64Variable',
+    'Int64Constant',
+    'binarize',
 ]
--- tensorflow_encrypted/tensor/int32.py
+++ tensorflow_encrypted/tensor/int32.py
@@ -5,14 +5,13 @@ import tensorflow as tf
 from typing import Union, Optional, List, Dict, Any, Tuple, Type
 from .tensor import AbstractTensor, AbstractVariable, AbstractConstant, AbstractPlaceholder
 from .factory import AbstractFactory
-from .prime import PrimeTensor
 
 from ..config import run
 
 
 class Int32Tensor(AbstractTensor):
 
-    modulus = 2**31
+    modulus = 2**32
     int_type = tf.int32
 
     def __init__(self, value: Union[np.ndarray, tf.Tensor]) -> None:
@@ -37,45 +36,41 @@ class Int32Tensor(AbstractTensor):
 
     @staticmethod
     def sample_uniform(shape: List[int]) -> 'Int32Tensor':
-        # TODO[Morten] what should maxval be (account for negative numbers)?
-        return Int32Tensor(tf.random_uniform(shape=shape, dtype=tf.int32, maxval=2**31 - 1))
+        return Int32Tensor(tf.random_uniform(shape=shape, dtype=tf.int32, minval=tf.int32.min, maxval=tf.int32.max))
 
     def __repr__(self) -> str:
         return 'int32.Tensor(shape={})'.format(self.shape)
 
     @property
     def shape(self) -> Union[Tuple[int, ...], tf.TensorShape]:
-        if self.value is None:
-            raise Exception("Can't call 'shape' on a empty tensor")
-
         return self.value.shape
 
-    def __add__(self, other: 'Int32Tensor') -> 'Int32Tensor':
+    def __add__(self, other) -> 'Int32Tensor':
         return self.add(other)
 
-    def __sub__(self, other: 'Int32Tensor') -> 'Int32Tensor':
+    def __sub__(self, other) -> 'Int32Tensor':
         return self.sub(other)
 
-    def __mul__(self, other: 'Int32Tensor') -> 'Int32Tensor':
+    def __mul__(self, other) -> 'Int32Tensor':
         return self.mul(other)
 
     def __mod__(self, k: int) -> 'Int32Tensor':
         return self.mod(k)
 
-    def add(self, other: 'Int32Tensor') -> 'Int32Tensor':
-        x, y = _lift(self), _lift(other)
+    def add(self, other) -> 'Int32Tensor':
+        x, y = Int32Tensor.lift(self), Int32Tensor.lift(other)
         return Int32Tensor(x.value + y.value)
 
-    def sub(self, other: 'Int32Tensor') -> 'Int32Tensor':
-        x, y = _lift(self), _lift(other)
+    def sub(self, other) -> 'Int32Tensor':
+        x, y = Int32Tensor.lift(self), Int32Tensor.lift(other)
         return Int32Tensor(x.value - y.value)
 
-    def mul(self, other: 'Int32Tensor') -> 'Int32Tensor':
-        x, y = _lift(self), _lift(other)
+    def mul(self, other) -> 'Int32Tensor':
+        x, y = Int32Tensor.lift(self), Int32Tensor.lift(other)
         return Int32Tensor(x.value * y.value)
 
-    def dot(self, other: 'Int32Tensor') -> 'Int32Tensor':
-        x, y = _lift(self), _lift(other)
+    def dot(self, other) -> 'Int32Tensor':
+        x, y = Int32Tensor.lift(self), Int32Tensor.lift(other)
         return Int32Tensor(tf.matmul(x.value, y.value))
 
     def im2col(self, h_filter, w_filter, padding, strides) -> 'Int32Tensor':
@@ -105,31 +100,6 @@ class Int32Tensor(AbstractTensor):
     def concat(xs: List['Int32Tensor'], axis: int) -> 'Int32Tensor':
         assert all(isinstance(x, Int32Tensor) for x in xs)
         return Int32Tensor(tf.concat([x.value for x in xs], axis=axis))
-
-    def binarize(self, prime=37) -> PrimeTensor:
-        BITS = 32
-        assert prime > BITS, prime
-
-        final_shape = [1] * len(self.shape) + [BITS]
-        bitwidths = tf.range(BITS, dtype=tf.int32)
-        bitwidths = tf.reshape(bitwidths, final_shape)
-
-        val = tf.expand_dims(self.value, -1)
-        val = tf.bitwise.bitwise_and(tf.bitwise.right_shift(val, bitwidths), 1)
-
-        return PrimeTensor.from_native(val, prime)
-
-
-def _lift(x: Union[Int32Tensor, int]) -> Int32Tensor:
-    # TODO[Morten] support other types of `x`
-
-    if isinstance(x, Int32Tensor):
-        return x
-
-    if type(x) is int:
-        return Int32Tensor.from_native(np.array([x]))
-
-    raise TypeError("Unsupported type {}".format(type(x)))
 
 
 class Int32Constant(Int32Tensor, AbstractConstant):
@@ -181,6 +151,7 @@ class Int32Variable(Int32Tensor, AbstractVariable):
 
 
 class Int32Factory(AbstractFactory):
+
     @property
     def Tensor(self) -> Type[Int32Tensor]:
         return Int32Tensor
