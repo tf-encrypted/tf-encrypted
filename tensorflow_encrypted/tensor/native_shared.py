6e47ea820f68f1474af92ab5832bd5753d477f16
Ben DeCoste <bendecoste@gmail.com>
2018-09-25 11:52:37 -0300

    name scope binarize function
    binarize crowds the graph in tensorboard quite a bit, name scoping
    it makes it a lot easier to read

--- tensorflow_encrypted/tensor/native_shared.py
+++ tensorflow_encrypted/tensor/native_shared.py
@@ -5,14 +5,15 @@ from .prime import PrimeTensor
 
 
 def binarize(tensor: AbstractTensor, prime: int=37) -> PrimeTensor:
-    BITS = tensor.int_type.size * 8
-    assert prime > BITS, prime
+    with tf.name_scope('binarize'):
+        BITS = tensor.int_type.size * 8
+        assert prime > BITS, prime
 
-    final_shape = [1] * len(tensor.shape) + [BITS]
-    bitwidths = tf.range(BITS, dtype=tf.int32)
-    bitwidths = tf.reshape(bitwidths, final_shape)
+        final_shape = [1] * len(tensor.shape) + [BITS]
+        bitwidths = tf.range(BITS, dtype=tf.int32)
+        bitwidths = tf.reshape(bitwidths, final_shape)
 
-    val = tf.expand_dims(tensor.value, -1)
-    val = tf.bitwise.bitwise_and(tf.bitwise.right_shift(val, bitwidths), 1)
+        val = tf.expand_dims(tensor.value, -1)
+        val = tf.bitwise.bitwise_and(tf.bitwise.right_shift(val, bitwidths), 1)
 
-    return PrimeTensor.from_native(val, prime)
+        return PrimeTensor.from_native(val, prime)
