233054ffe91e1bed0d4c5098f149875bc28c2192
Justin Patriquin <justin@sidestory.email>
2018-10-02 09:25:46 -0300

    lint

--- tensorflow_encrypted/tensor/odd_implicit.py
+++ tensorflow_encrypted/tensor/odd_implicit.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import numpy as np
 import tensorflow as tf
-from typing import Union, Optional, List, Dict, Any, Tuple, Type
+from typing import Union, Optional, List, Dict, Any, Tuple
 
 from ..config import run
 from .tensor import AbstractTensor
