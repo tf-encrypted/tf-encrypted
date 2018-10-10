00f66cb4bec4023273a3f714ed7df5f9b1d98d0e
Ian Livingstone <ianl@cs.dal.ca>
2018-10-10 16:57:07 -0300

    Upgrade to tensorflow 1.11.0
    A new version of Tensorflow is available, so let's code to the latest
    greatest while we're still in our unstable alpha stage!

--- setup.py
+++ setup.py
@@ -9,12 +9,11 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
     install_requires=[
-        "tensorflow>=1.9.0",
+        "tensorflow>=1.11.0",
         "numpy>=1.14.0"
     ],
     extra_requires={
-        "tf": ["tensorflow>=1.10.0"],
-        "tf_gpu": ["tensorflow-gpu>=1.10.0"]
+        "tf": ["tensorflow>=1.11.0"]
     },
     license="Apache License 2.0",
     url="https://github.com/mortendahl/tf-encrypted",
