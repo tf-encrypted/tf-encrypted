c3a8482ed41dfd6f2bcc283877f38997f08fb332
Justin Patriquin <justin@sidestory.email>
2018-11-01 15:42:33 -0300

    secure random docs

--- tf_encrypted/operations/secure_random/secure_random.py
+++ tf_encrypted/operations/secure_random/secure_random.py
@@ -9,7 +9,22 @@ shared_object = dirname + '/operations/secure_random/secure_random_module.so'
 
 
 def secure_random(shape, minval=0, maxval=None, dtype=tf.int32, seed=None, name=None):
-    # TODO docs
+    """
+    Returns random numbers securely.
+
+    .. code-block:: python
+
+        x = secure_random([2, 2], minval=-1000, maxval=1000)
+
+    :param list shape: Shape of the random tensor.
+    :param int minval: Minimum value to return, inclusive.
+    :param int maxval: Maximum value to return, exclusive.
+    :param dtype: Data type of the return random values. Either int32 or int64.
+    :param tf.Tensor seed: The seed to be used when generating the random numbers.
+    :param str name:
+
+    :rtype: tf.Tensor
+    """
 
     secure_random_module = tf.load_op_library(shared_object)
 
