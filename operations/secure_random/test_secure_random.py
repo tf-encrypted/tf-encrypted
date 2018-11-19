43659da3353b5878d4e54feac69027cc1605a88a
Justin Patriquin <justin@sidestory.email>
2018-11-19 13:33:23 -0400

    make sure negative numbers are returned

--- operations/secure_random/test_secure_random.py
+++ operations/secure_random/test_secure_random.py
@@ -8,13 +8,12 @@ import os
 
 dirname = os.path.dirname(tfe.__file__)
 shared_object = dirname + '/operations/secure_random/secure_random_module.so'
+secure_random_module = tf.load_op_library(shared_object)
 
 
 class TestSecureRandom(unittest.TestCase):
 
     def test_int32_return(self):
-        secure_random_module = tf.load_op_library(shared_object)
-
         expected = [[749, 945, 451], [537, 795, 111]]
 
         with tf.Session():
@@ -23,8 +22,6 @@ class TestSecureRandom(unittest.TestCase):
             np.testing.assert_array_equal(output, expected)
 
     def test_int64_return(self):
-        secure_random_module = tf.load_op_library(shared_object)
-
         expected = [[469, 403, 651], [801, 843, 806]]
 
         with tf.Session():
@@ -36,8 +33,6 @@ class TestSecureRandom(unittest.TestCase):
             np.testing.assert_array_equal(output, expected)
 
     def test_min_max_range(self):
-        secure_random_module = tf.load_op_library(shared_object)
-
         with tf.Session():
             minval = tf.constant(-100000000, dtype=tf.int32)
             maxval = tf.constant(100000000, dtype=tf.int32)
@@ -49,13 +44,22 @@ class TestSecureRandom(unittest.TestCase):
 
     def test_invalid_max_min(self):
         with tf.Session():
-            secure_random_module = tf.load_op_library(shared_object)
-
             minval = tf.constant(1000, dtype=tf.int64)
             maxval = tf.constant(-1000, dtype=tf.int64)
 
             with np.testing.assert_raises(errors.InvalidArgumentError):
                 secure_random_module.secure_random([2, 3], [1, 1, 1, 1, 1, 1, 1, 2], minval, maxval).eval()
+
+    def test_negative_numbers(self):
+        expected = [[-1531, -1597, -1349], [-1199, -1157, -1194]]
+
+        with tf.Session():
+            minval = tf.constant(-2000, dtype=tf.int64)
+            maxval = tf.constant(-1000, dtype=tf.int64)
+
+            output = secure_random_module.secure_random([2, 3], [1, 1, 1, 1, 1, 1, 1, 2], minval, maxval).eval()
+
+            np.testing.assert_array_equal(output, expected)
 
 
 if __name__ == '__main__':
