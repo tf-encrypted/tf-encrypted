47c5c4501057e33656e65b9472b7d057f5ff8927
jvmancuso <jason@partydb.io>
2018-11-26 09:48:37 -0500

    Player documentation

--- tf_encrypted/player/__main__.py
+++ tf_encrypted/player/__main__.py
@@ -6,8 +6,10 @@ if __name__ == '__main__':
     import tf_encrypted as tfe
 
     parser = argparse.ArgumentParser(description="Run a tf-encrypted player")
-    parser.add_argument('name', metavar='NAME', type=str, help='name of player as specified in the config file')
-    parser.add_argument('--config', metavar='FILE', type=str, help='path to configuration file', default='./config.json')
+    parser.add_argument('name', metavar='NAME', type=str,
+                        help='name of player as specified in the config file')
+    parser.add_argument('--config', metavar='FILE', type=str,
+                        help='path to configuration file', default='./config.json')
     args = parser.parse_args()
 
     config = tfe.config.load(args.config)
