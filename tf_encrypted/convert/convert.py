9352cdd8a3e55433e897205c408d21aa8ae6b0b0
yann dupis <yann.dupis@gmail.com>
2018-12-10 16:44:13 +0100

    support several occurence of the special ops

--- tf_encrypted/convert/convert.py
+++ tf_encrypted/convert/convert.py
@@ -7,7 +7,6 @@ from ..protocol.pond import TFEInputter
 from ..config import Config, get_config
 
 special_ops = ['required_space_to_batch_paddings']
-special_op_two_outputs = ['required_space_to_batch_paddings']
 
 class Converter():
 
@@ -46,11 +45,13 @@ class Converter():
             inputs = [inputter_fn]
         inputs_iterable = enumerate(inputs)
 
+
         # Identify if there are special ops in pb file, e.g. required_space_to_batch_paddings 
         # If yes, identify the inputs and outputs of this special ops.
-        if are_there_special_ops(special_ops[0], graph_def):
-            input_special_op = find_inputs(special_ops[0], graph_def)
-            output_special_op = find_outputs(special_ops[0], graph_def)
+        special_op_dict, all_special_op_inputs, all_special_op_outputs = find_special_ops(special_ops[0], graph_def)
+
+        # Check if there are special ops in the graph, if not, use the existing approach.
+        if len(special_op_dict.keys()) != 0:
 
             # Create a dictionary excluding all the sub ops related to required_space_to_batch_paddings
             # Except the sub ops related to the input or output of this special ops.
@@ -58,19 +59,18 @@ class Converter():
 
             for n in graph_def.node:
                 if special_ops[0] in n.name: 
-                    if  n.name in input_special_op or n.name in output_special_op:
+                    if  n.name in all_special_op_inputs or n.name in all_special_op_outputs:
                         pb_trimmed[n.name] = n
                 else:
                     pb_trimmed[n.name] = n
 
             node_list = pb_trimmed.values()
 
-
-            # If the ops are not related to the special ops, use the existing approach to regist the ops. 
+            # If the ops are not related to the special ops, use the existing approach to register the ops. 
             # Otherwise for the special ops replace the output from the sub ops by the output from the 
             # high level operation then register. 
             for node in node_list:
-                if node.name not in output_special_op:
+                if node.name not in all_special_op_outputs:
 
                     output = node_name(node.name)
                     inputs = [node_name(x) for x in node.input]
@@ -87,13 +87,17 @@ class Converter():
 
                     self.outputs[output] = register[node.op](self, node, inputs)
                 else:
-                    # Handle edge cased if the ops return two outputs
-                    if special_ops[0] in special_op_two_outputs :
-                        a, b = register[special_ops[0]](self, node, input_special_op)
-                        self.outputs[output_special_op[0]] = a
-                        self.outputs[output_special_op[1]] = b
-                    else:
-                        self.outputs[node.name] = register[special_ops[0]](self, node, inputs)
+                    # Register high level special operations
+                    for s in special_op_dict.keys():
+                        input_list, output_list = special_op_dict[s]['inputs'], special_op_dict[s]['outputs']
+
+                         # Handle edge cased if the ops return two outputs
+                        if len(output_list) == 2:
+                            a, b = register[special_op_dict[s]['op']](self, node, input_list)
+                            self.outputs[output_list[0]] = a
+                            self.outputs[output_list[1]] = b
+                        else:
+                            self.outputs[output_list[0]] = register[special_ops[0]](self, node, inputs)
 
         else:
             for node in graph_def.node:
@@ -162,9 +166,52 @@ def find_outputs(special_ops, graph_def):
     final_list = list(final_output.keys())
     return final_list
 
+
 def are_there_special_ops(special_op, graph_def):
     for n in graph_def.node:
         if special_op in n.name:
             return True
         
     return False
+
+
+def special_ops_name_space(special_ops_name, graph):
+
+    special_op_name_space = set()
+    for n in graph.node:
+        name = n.name.split('/')
+
+        special_op_idx = 0
+
+        for j in range(len(name)):
+            if special_ops_name in name[j]:
+                special_op_name_space.add('/'.join(name[:special_op_idx + 1]))
+
+    return list(special_op_name_space)
+
+
+def find_special_ops(special_ops_name, graph):
+  
+    special_ops_name_space_list = special_ops_name_space(special_ops_name, graph)
+    
+    special_op_dict = OrderedDict()
+    all_special_op_inputs = []
+    all_special_op_outputs = []
+    
+    for n in special_ops_name_space_list:
+        
+        special_op_dict[n] = OrderedDict()
+        
+        special_op_dict[n]['op'] = special_ops_name
+        
+        inputs = find_inputs(n, graph)
+        special_op_dict[n]['inputs'] = inputs
+        all_special_op_inputs += inputs
+    
+    
+        outputs = find_outputs(n, graph)
+        special_op_dict[n]['outputs'] = outputs
+        all_special_op_outputs += outputs
+    
+    return special_op_dict, all_special_op_inputs, all_special_op_outputs
+    
