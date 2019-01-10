519d6525fd4bfd661073e50ec3f0f2432a7042f8
Morten Dahl <mortendahlcs@gmail.com>
2019-01-10 15:02:11 +0100

    make existing tests conditional on whether secure_random is supported

--- tests/test_secure_random.py
+++ tests/test_secure_random.py
@@ -1,12 +1,15 @@
 import unittest
 
-import tensorflow as tf
 import numpy as np
-from tf_encrypted.operations.secure_random import seeded_random_uniform, random_uniform, seed as seed_gen
+import tensorflow as tf
 
-seed = [87654321, 87654321, 87654321, 87654321, 87654321, 87654321, 87654321, 87654321]
+from tf_encrypted.operations import secure_random
 
 
+SEED = [87654321, 87654321, 87654321, 87654321, 87654321, 87654321, 87654321, 87654321]
+
+
+@unittest.skipUnless(secure_random.supports_seeded_randomness(), "Secure random disabled")
 class TestSeededRandom(unittest.TestCase):
 
     def test_wrapper(self):
@@ -15,7 +18,7 @@ class TestSeededRandom(unittest.TestCase):
                     [9678, 7188, 8280]]
 
         with tf.Session():
-            output = seeded_random_uniform([3, 3], seed=seed, maxval=10000).eval()
+            output = secure_random.seeded_random_uniform([3, 3], seed=SEED, maxval=10000).eval()
 
             np.testing.assert_array_equal(output, expected)
 
@@ -25,7 +28,7 @@ class TestSeededRandom(unittest.TestCase):
                     [9356, 4377, 6561]]
 
         with tf.Session():
-            output = seeded_random_uniform([3, 3], seed=seed, minval=-10000, maxval=10000).eval()
+            output = secure_random.seeded_random_uniform([3, 3], seed=SEED, minval=-10000, maxval=10000).eval()
 
             np.testing.assert_array_equal(output, expected)
 
@@ -33,15 +36,15 @@ class TestSeededRandom(unittest.TestCase):
         with tf.Session():
             # invalid seed
             with np.testing.assert_raises(ValueError):
-                seeded_random_uniform([3, 3], maxval=10000, seed=[1]).eval()
+                secure_random.seeded_random_uniform([3, 3], maxval=10000, seed=[1]).eval()
 
             # invalid maxval
             with np.testing.assert_raises(ValueError):
-                seeded_random_uniform([3, 3]).eval()
+                secure_random.seeded_random_uniform([3, 3]).eval()
 
             # invalid dtype
             with np.testing.assert_raises(ValueError):
-                seeded_random_uniform([3, 3], seed=seed, maxval=10000, dtype=tf.float32).eval()
+                secure_random.seeded_random_uniform([3, 3], seed=SEED, maxval=10000, dtype=tf.float32).eval()
 
     def test_rejection(self):
         m = 1129
@@ -49,23 +52,24 @@ class TestSeededRandom(unittest.TestCase):
         seed0 = [2108217960, -1340439062, 476173466, -681389164, -1502583120, 1663373136, 2144760032, -1591917499]
 
         with tf.Session():
-            out0 = seeded_random_uniform([64, 4500], seed=seed0, maxval=m, dtype=tf.int32).eval()
-            out1 = seeded_random_uniform([64, 4500], seed=seed0, maxval=m, dtype=tf.int32).eval()
+            out0 = secure_random.seeded_random_uniform([64, 4500], seed=seed0, maxval=m, dtype=tf.int32).eval()
+            out1 = secure_random.seeded_random_uniform([64, 4500], seed=seed0, maxval=m, dtype=tf.int32).eval()
 
             np.testing.assert_array_equal(out0, out1)
 
 
+@unittest.skipUnless(secure_random.supports_secure_randomness(), "Secure random disabled")
 class TestRandomUniform(unittest.TestCase):
 
     def test_wrapper(self):
         with tf.Session():
-            output = random_uniform([3, 3], maxval=10000).eval()
+            output = secure_random.random_uniform([3, 3], maxval=10000).eval()
 
             np.testing.assert_array_equal(output.shape, [3, 3])
 
     def test_min_val(self):
         with tf.Session():
-            output = random_uniform([6], minval=-10000, maxval=0).eval()
+            output = secure_random.random_uniform([6], minval=-10000, maxval=0).eval()
 
             for out in output:
                 assert(out < 0)
@@ -74,25 +78,28 @@ class TestRandomUniform(unittest.TestCase):
         with tf.Session():
             # invalid maxval
             with np.testing.assert_raises(ValueError):
-                random_uniform([3, 3]).eval()
+                secure_random.random_uniform([3, 3]).eval()
 
             # invalid dtype
             with np.testing.assert_raises(ValueError):
-                random_uniform([3, 3], maxval=10000, dtype=tf.float32).eval()
+                secure_random.random_uniform([3, 3], maxval=10000, dtype=tf.float32).eval()
 
 
+@unittest.skipUnless(secure_random.supports_seeded_randomness(), "Secure random disabled")
 class TestSeed(unittest.TestCase):
-    with tf.Session():
-        s = seed_gen()
 
-        minval = -2000
-        maxval = 0
+    def test_seed_generation(self):
+        with tf.Session():
+            s = secure_random.seed()
 
-        shape = [2, 3]
+            minval = -2000
+            maxval = 0
 
-        output = seeded_random_uniform(shape, seed=s, minval=minval, maxval=maxval).eval()
+            shape = [2, 3]
 
-        np.testing.assert_array_equal(output.shape, shape)
+            output = secure_random.seeded_random_uniform(shape, seed=s, minval=minval, maxval=maxval).eval()
+
+            np.testing.assert_array_equal(output.shape, shape)
 
 
 if __name__ == '__main__':
