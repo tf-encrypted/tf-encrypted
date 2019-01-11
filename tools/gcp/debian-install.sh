1cea7bf2a1209185d4e8d8908589cd3cdd3b8e06
Morten Dahl <mortendahlcs@gmail.com>
2019-01-11 14:54:03 +0100

    refresh of gcp install script

--- tools/gcp/debian-install.sh
+++ tools/gcp/debian-install.sh
@@ -3,15 +3,16 @@
 echo 'export LANGUAGE=en_US.UTF-8' >> .bashrc
 echo 'export LANG=en_US.UTF-8' >> .bashrc
 echo 'export LANG=en_US.UTF-8' >> .bashrc
+echo 'export LC_ALL=C' >> .bashrc
 
 # install OS packages
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get install -y python3-pip 
-sudo apt-get install -y git
-sudo apt-get install -y screen # for running a tf server
-sudo apt-get install -y psmisc # for killall
-sudo apt-get install -y tcptrack iftop # for network monitoring
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y python3-pip 
+sudo apt install -y git
+sudo apt install -y screen # for running a tf server
+sudo apt install -y psmisc # for killall
+sudo apt install -y tcptrack iftop # for network monitoring
 
 # install tensorflow-encrypted
 git clone https://github.com/mortendahl/tf-encrypted.git
