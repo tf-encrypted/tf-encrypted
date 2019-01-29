74df271d4d2a0111ae0cec9da1ebf8ac406ec499
Benjamin DeCoste <bendecoste@gmail.com>
2019-01-29 09:57:30 -0400

    added secure_model to private_model (#393)

---
+++ tests/test_secure_model.py
@@ -0,0 +1,36 @@
+import unittest
+
+import numpy as np
+import tensorflow as tf
+import tf_encrypted as tfe
+from tf_encrypted.private_model import secure_model
+
+
+class TestSecureModel(unittest.TestCase):
+
+    def setUp(self):
+        tf.reset_default_graph()
+
+    def tearDown(self):
+        tf.reset_default_graph()
+
+    def test_secure_model(self):
+        with tfe.protocol.Pond():
+            tf.random.set_random_seed(42)
+
+            d = tf.keras.layers.Dense(1, input_shape=(10,), use_bias=False)
+            model = tf.keras.Sequential([
+                d
+            ])
+
+            input = np.ones((1, 10))
+            output = model.predict(input)
+
+            s_model = secure_model(model)
+            s_output = s_model.private_predict(input)
+
+            np.testing.assert_array_almost_equal(s_output, output, 4)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tf_encrypted/private_model.py
+++ tf_encrypted/private_model.py
@@ -1,13 +1,18 @@
 import tensorflow as tf
 import tf_encrypted as tfe
 
+from tensorflow.keras import backend as K
+from tensorflow.python.framework import graph_util
+from tensorflow.python.platform import gfile
+from tensorflow.python.framework.graph_util_impl import remove_training_nodes
+
 
 class PrivateModel():
     def __init__(self, output_node):
         self.output_node = output_node
 
     # TODO support multiple inputs
-    def predict(self, input):
+    def private_predict(self, input):
         name = "private-input/api/0:0"
         pl = tf.get_default_graph().get_tensor_by_name(name)
 
@@ -21,3 +26,47 @@ class PrivateModel():
             )
 
             return output
+
+
+def load_graph(model_file):
+
+    input_spec = []
+    with gfile.GFile(model_file, 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+
+        for node in graph_def.node:
+            if node.op != "Placeholder":
+                continue
+
+            input_spec.append({
+                'name': node.name,
+                'dtype': node.attr['dtype'].type,
+                'shape': [1] + [int(d.size) for d in node.attr['shape'].shape.dim[1:]]
+            })
+
+    inputs = []
+    for i, spec in enumerate(input_spec):
+        def scope(i, spec):
+            def provide_input() -> tf.Tensor:
+                pl = tf.placeholder(tf.float32, shape=spec['shape'], name="api/{}".format(i))
+                return pl
+
+            return provide_input
+
+        inputs.append(scope(i, spec))
+
+    return graph_def, inputs
+
+
+def secure_model(model):
+    session = K.get_session()
+    min_graph = graph_util.convert_variables_to_constants(session, session.graph_def, [node.op.name for node in model.outputs])
+    tf.train.write_graph(min_graph, '/tmp', 'model.pb', as_text=False)
+
+    graph_def, inputs = load_graph('/tmp/model.pb')
+
+    c = tfe.convert.convert.Converter()
+    y = c.convert(remove_training_nodes(graph_def), tfe.convert.register(), 'input-provider', inputs)
+
+    return PrivateModel(y)
