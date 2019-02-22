b95e2a8f9df0459965971d0a3da3b0710b19c6e0
Justin Patriquin <justinpatriquin@gmail.com>
2019-02-22 09:07:25 -0400

    share

--- tf_encrypted/protocol/replicated/replicated.py
+++ tf_encrypted/protocol/replicated/replicated.py
@@ -1,101 +1,42 @@
-from __future__ import absolute_import
 from typing import Optional, Tuple, List
 import abc
 import sys
 import math
+import enum
 
 import tensorflow as tf
 
-from ..protocol import memoize, Protocol
-from ...tensor.factory import AbstractTensor
-from ...player import Player
-from ...config import get_config
+from tf_encrypted.operations.secure_random import seed, seeded_random_uniform
 
 
-class ReplicatedTensor(abc.ABC):
-    pass
+#
+#
+# class ReplicatedPublicTensor(ReplicatedTensor):
+#     pass
+#
+#
 
-class ReplicatedPublicTensor(ReplicatedTensor):
-    pass
+#
+#
+# class Tensor(abc.ABC):
+#
+#     @property
+#     def extended_dtype(self):
+#         return (self.dtype, self.protection)
 
-class ReplicatedPrivateTensor(ReplicatedTensor):
-
-    dispatch_id = 'private'
-
-    def __init__(
-        self,
-        players,
-        shares,
-    ) -> None:
-        # assert share0.shape.as_list() == share1.shape.as_list() == share2.shape.as_list()
-
-        self.players = players
-        self.shares = shares
-
-    # @property
-    # def shape(self) -> List[int]:
-    #     return self.share0.shape.as_list()
-
-
-def _private_input(prot, player, input_fn) -> ReplicatedPrivateTensor:
-    pass
-
-
-class Kernel:
-    pass
-
-class AddPrivatePrivate(Kernel):
-    pass
-
-Fixed10 = Fixed(10)
-Fixed20 = Fixed(20)
-
-fixed:
-    - precision (interpretation)
-    - backing (delegation)
-    - public/private (representation)
-    - players (delegation)
-    - replicated/shared/.. (representation)
-
-int:
-    - backing (delegation)
-    - public/private (representation)
-    - players (property)
-    - replicated/shared/..
-
-Pond: fixed16, two_additive
-BEDK19: quan8, three_replicated
-ABY3: fixed12, three_replicated
-
-# upscale
-Convert<Fixed(p), Fixed(q)> forall q > p >= 0
-
-# truncate
-Convert<Fixed(p), Fixed(q)> forall 0 <= q < p
-
-Add<X: Fixed(p), Y: Fixed(q)> :=
-    Convert<Fixed(p), Fixed(q)>
-
-Add<X: Fixed(p), Y: Fixed(p)> :=
-    Add<Int, Int>
-
-class Add
-
-
-_add_private_private = AddPrivatePrivate(Fixed, Fixed)
-
-class Tensor(abc):
-
-    @property
-    def extended_dtype(self):
-        return (self.dtype, self.protection)
-
-class Private(abc):
-    pass
+# class ThreeReplicated(Tensor):
+#
+#     @property
+#     def dtype(self):
+#         return self._dtype
+#
+#     @property
+#     def dtype_extended(self):
+#         return (self.dtype, replicated_three)
 
 @enum.unique
 class Protection(enum.Enum):
-    
+
     # values that can be freely revealed to anyone
     PUBLIC = 1
 
@@ -105,124 +46,83 @@ class Protection(enum.Enum):
     # values intended to only be shared in encrypted form
     ENCRYPTED = 3
 
-class Public(Tensor): # can be revealed
-    backing
-    dtype: int64, fixed10
-    protection: public
 
-class ThreeReplicated(Tensor):
+maxval = 100
 
-    @property
-    def dtype(self):
-        return self._dtype
 
-    @property
-    def dtype_extended(self):
-        return (self.dtype, replicated_three)
-    
+def zero_share(players, shape):
+    player1, player2, player3 = players
 
-    backing
-    dtype: int64, fixed10
-    protection: encrypted
+    with tf.device(player3.device_name):
+        s2 = seed()
 
-class PrivateRing:
+    with tf.device(player2.device_name):
+        s1 = seed()
+        alpha1 = seeded_random_uniform(shape, seed=s1, maxval=maxval) - seeded_random_uniform(shape, seed=s2, maxval=maxval)
+
+    with tf.device(player1.device_name):
+        s0 = seed()
+        alpha0 = seeded_random_uniform(shape, seed=s0, maxval=maxval) - seeded_random_uniform(shape, seed=s1, maxval=maxval)
+
+    with tf.device(player3.device_name):
+        alpha2 = seeded_random_uniform(shape, seed=s2, maxval=maxval) - seeded_random_uniform(shape, seed=s0, maxval=maxval)
+
+    return alpha0, alpha1, alpha2
+
+
+def share(players, x):
+    alpha0, alpha1, alpha2 = zero_share(players, x.shape)
+
+    x1 = alpha1
+    x2 = alpha2
+
+    with tf.device(players[0].device_name):
+        x0 = x + alpha0
+
+    xs = ReplicatedPrivateTensor(players, ((None, x1, x2), (x0, None, x2), (x0, x2, None)))
+
+    return xs
+
+
+def recombine(players, z):
+    with tf.device(players[0].device_name):
+        final = z.shares[0][1] + z.shares[1][2] + z.shares[2][0]
+
+    return final
+
+
+class ReplicatedTensor(abc.ABC):
     pass
 
-class AdditiveN:
-    shares: Tuple[BackingTensor]
 
-class Additive2:
-    shares: Pair[BackingTensor]
+class ReplicatedPrivateTensor(ReplicatedTensor):
+    def __init__(
+        self,
+        players,
+        shares,
+    ) -> None:
+        self.players = players
+        self.shares = shares
 
-class Replicated3:
-    shares: Tuple[BackingTensor]
-
-class PrivateInteger:
-    element: PrivateRing
-
-class PrivateFixedpoint<ER>:
-    precision: int
-    encoded: ER
-
-class PrivateQuantized<ER>:
-    precision: int
-    zero_point: ER
-    scale: ER
-
-backing:
-- ring32
-- ring64
-- ring100
-
-encrypted:
-- additive2
-- additive2_masked
-- replicated3
-
-dtype:
-- int32
-- int64
-- fixed16
-- quant8
-
-add<ring64, ring64>
-add<ring64, int>
-add<(additive2, ring32), (additive2, ring32)>
-add<(additive2, ring64), (additive2, ring64)>
-add<(additive2, ring32), (additive2, ring32)>
-
-for backing in [..]:
-    ops.put(add, (fixed16, additive2, backing), (fixed16, additive2, backing))
-
-add<(fixed16, additive2, ring64), (fixed16, additive2, ring64)>
-dot<(quant8, replicated3, ring64), (quant8, replicated3, ring64)>
-
-add<PrivateInteger, PrivateFixedpoint>
-
-
-
-
-class Additive(Tensor):
-    backing
-    dtype: int64, fixed10
-    protection: encrypted
-
-
-class Tensor
-
-class PrivateTensor
-class PublicTensor
-
-class IntPrivateReplicated(Int64)
-class IntPublicReplicated(Int64)
-class FixedPrivateReplicated(Int64)
-class FixedPublicReplicated(Int64)
-
-class IntPrivateAdditive(Int64)
-class IntPublicAdditive(Int64)
-class FixedPrivateAdditive(Int64)
-class FixedPublicAdditive(Int64)
-
-
-int64 = IntPublic
-pint64 
+    @property
+    def shape(self):
+        # cache shape?!
+        for share in self.shares[0]:
+            if share is not None:
+                return share[0].shape
 
 
 class Kernel:
-
     def __call__(self, *args):
-        return self.run(*args)
+        pass
+
 
 class AddPrivatePrivate(Kernel):
-
-    @property
-    def signature(self):
-        return 
-
     def __call__(self, x: ReplicatedPrivateTensor, y: ReplicatedPrivateTensor) -> ReplicatedPrivateTensor:
-        players = x.players; assert y.players == players
+        players = x.players
+        assert y.players == players
         player0, player1, player2 = players
-        
+
         x_on_0, x_on_1, x_on_2 = x.shares
         y_on_0, y_on_1, y_on_2 = y.shares
 
@@ -245,19 +145,14 @@ class AddPrivatePrivate(Kernel):
 
         return ReplicatedPrivateTensor(players, shares)
 
-add = Dispatcher(
-    AddPrivatePrivate(),
-    AddPrivatePublic(),
-    AddPublicPrivate(),
-    AddPublicPublic(),
-)
 
 class SubPrivatePrivate(Kernel):
-    
+
     def __call__(self, x: ReplicatedPrivateTensor, y: ReplicatedPrivateTensor) -> ReplicatedPrivateTensor:
-        players = x.players; assert y.players == players
+        players = x.players
+        assert y.players == players
         player0, player1, player2 = players
-        
+
         x_on_0, x_on_1, x_on_2 = x.shares
         y_on_0, y_on_1, y_on_2 = y.shares
 
@@ -280,24 +175,19 @@ class SubPrivatePrivate(Kernel):
 
         return ReplicatedPrivateTensor(players, shares)
 
-sub = Dispatcher(
-    SubPrivatePrivate(),
-    SubPrivatePublic(),
-    SubPublicPrivate(),
-    SubPublicPublic(),
-)
 
 class MulPrivatePrivate(Kernel):
 
     def __call__(self, x: ReplicatedPrivateTensor, y: ReplicatedPrivateTensor) -> ReplicatedPrivateTensor:
-        players = x.players; assert y.players == players
+        players = x.players
+        assert y.players == players
         player0, player1, player2 = players
 
         x_on_0, x_on_1, x_on_2 = x.shares
         y_on_0, y_on_1, y_on_2 = y.shares
 
         # get the next zero triple defined by seeds (s0, s1, s2) shared between the three players
-        alpha0, alpha1, alpha2 = (5, 7, -12)
+        alpha0, alpha1, alpha2 = zero_share(players, x.shape)
 
         with tf.device(player0.device_name):
             z2_on_0 = (x_on_0[2] * y_on_0[2] +
@@ -323,10 +213,3 @@ class MulPrivatePrivate(Kernel):
         shares = (z0, z1, z2)
 
         return ReplicatedPrivateTensor(players, shares)
-
-mul = Dispatcher(
-    MulPrivatePrivate(),
-    MulPrivatePublic(),
-    MulPublicPrivate(),
-    MulPublicPublic(),
-)
--- tf_encrypted/protocol/replicated/replicated_test.py
+++ tf_encrypted/protocol/replicated/replicated_test.py
@@ -1,16 +1,33 @@
 import unittest
 
+import tensorflow as tf
+
 import tf_encrypted as tfe
 import numpy as np
 
-from .replicated import (
+from replicated import (
     ReplicatedPrivateTensor,
-    _add_private_private,
-    _mul_private_private,
+    AddPrivatePrivate,
+    MulPrivatePrivate,
+    zero_share,
+    share,
+    recombine,
 )
 
 
 class TestReplicated(unittest.TestCase):
+
+    def test_zero_share(self):
+        tfe.set_config(tfe.LocalConfig([
+            'server0', 'server1', 'server2'
+        ]))
+
+        players = tfe.get_config().get_players('server0, server1, server2')
+        alpha0, alpha1, alpha2 = zero_share(players, [5, 5])
+
+        out = alpha0 + alpha1 + alpha2
+
+        np.testing.assert_equal(tfe.Session().run(out), np.zeros([5, 5]))
 
     def test_add(self):
 
@@ -20,15 +37,16 @@ class TestReplicated(unittest.TestCase):
 
         players = tfe.get_config().get_players('server0, server1, server2')
 
-        x = ReplicatedPrivateTensor(players, ((None,1,2), (0,None,2), (0,1,None)))
-        y = ReplicatedPrivateTensor(players, ((None,1,2), (0,None,2), (0,1,None)))
+        x = share(players, np.array([3, 4, 5]))
+        y = share(players, np.array([3, 4, 5]))
 
-        z = _add_private_private(x, y)
+        kern = AddPrivatePrivate()
 
-        assert z.shares[0][1] + z.shares[1][2] + z.shares[2][0] == 6
-        assert z.shares[0][0] is None
-        assert z.shares[1][1] is None
-        assert z.shares[2][2] is None
+        z = kern(x, y)
+
+        final = recombine(players, z)
+
+        np.testing.assert_equal(tfe.Session().run(final), [6, 8, 10])
 
     def test_mul(self):
 
@@ -38,15 +56,15 @@ class TestReplicated(unittest.TestCase):
 
         players = tfe.get_config().get_players('server0, server1, server2')
 
-        x = ReplicatedPrivateTensor(players, ((None,1,2), (0,None,2), (0,1,None)))
-        y = ReplicatedPrivateTensor(players, ((None,1,2), (0,None,2), (0,1,None)))
+        x = share(players, np.array([3, 4, 5]))
+        y = share(players, np.array([3, 4, 5]))
 
-        z = _mul_private_private(x, y)
+        kern = MulPrivatePrivate()
+        z = kern(x, y)
 
-        assert z.shares[0][1] + z.shares[1][2] + z.shares[2][0] == 9
-        assert z.shares[0][0] is None
-        assert z.shares[1][1] is None
-        assert z.shares[2][2] is None
+        final = recombine(players, z)
+
+        np.testing.assert_equal(tfe.Session().run(final), [9, 16, 25])
 
 
 if __name__ == '__main__':
