a93f74fdf4cff5aa1c6483df9bf53209508011de
jvmancuso <jason@dropoutlabs.com>
2019-04-16 17:10:53 -0400

    release 0.5.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tf-encrypted",
-    version="0.4.0",
+    version="0.5.1",
     packages=setuptools.find_packages(),
     package_data={'tf_encrypted': [
         'operations/secure_random/*.so',
