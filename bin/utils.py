18ec4a3104213f3c32ff2f8268520f3b27211b9d
yann dupis <yann.dupis@gmail.com>
2019-04-16 18:55:04 -0700

    point to data_dir with tfrecords

--- bin/utils.py
+++ bin/utils.py
@@ -2,16 +2,16 @@ import tensorflow as tf
 
 
 def data_prep_from_saved_model(
-    saved_model_dir, 
-    data_filenames, 
+    saved_model_dir,
+    data_filenames,
     batch_size,
     data_prep_start_node="serialized_example:0",
     data_prep_end_node="DatasetToSingleElement:0"
-    ):
+):
 
     # SavedModel with data pre-processing steps
     with tf.Session(graph=tf.Graph()) as sess:
-        tf.saved_model.loader.load(sess, ['serve'], saved_model_dir) 
+        tf.saved_model.loader.load(sess, ['serve'], saved_model_dir)
 
     # Extract graph definition
     gdef = sess.graph_def
@@ -23,22 +23,17 @@ def data_prep_from_saved_model(
     dataset_b = iterator.get_next()
 
     # Preprocess data
-    data_out, = tf.import_graph_def(
-                        gdef,
-                        input_map={data_prep_start_node: dataset_b},
-                        return_elements=[data_prep_end_node])
+    data_out, = tf.import_graph_def(gdef,
+                                    input_map={data_prep_start_node: dataset_b},
+                                    return_elements=[data_prep_end_node])
 
-    # TFE expects tensors with fully defined shape 
+    # TFE expects tensors with fully defined shape
     fixed_shape = [batch_size] + data_out.get_shape().as_list()[1:]
     data_out = tf.reshape(data_out, fixed_shape)
     return data_out
 
 
-
-
-
-
-
-
-
-
+def list_files_from_dir(directory):
+    file_names_list = tf.gfile.ListDirectory(directory)
+    path_files_list = [directory + f for f in file_names_list]
+    return path_files_list
