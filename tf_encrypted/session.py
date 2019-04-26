05f95778b07f9b2d3cd75cf452d0095693de35d7
Morten Dahl <mortendahlcs@gmail.com>
2019-04-26 15:59:21 +0200

    Flag to easily output partition graphs

--- tf_encrypted/session.py
+++ tf_encrypted/session.py
@@ -87,7 +87,8 @@ class Session(tf.Session):
         fetches: Any,
         feed_dict: Dict[tf.Tensor, np.ndarray] = {},
         tag: Optional[str] = None,
-        write_trace: bool = False
+        write_trace: bool = False,
+        output_partition_graphs: bool = False,
     ):
         """
         run(fetches, feed_dict, tag, write_trace) -> Any
@@ -123,7 +124,8 @@ class Session(tf.Session):
             _run_counter[tag] += 1
 
             writer = tf.summary.FileWriter(run_tag, self.graph)
-            run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+            run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE,
+                                        output_partition_graphs=output_partition_graphs)
             run_metadata = tf.RunMetadata()
 
             fetches_out = super(Session, self).run(
@@ -132,6 +134,14 @@ class Session(tf.Session):
                 options=run_options,
                 run_metadata=run_metadata
             )
+
+            if output_partition_graphs:
+                for i, g in enumerate(run_metadata.partition_graphs):
+                    tf.io.write_graph(
+                        g,
+                        logdir=os.path.join(__TENSORBOARD_DIR__, session_tag),
+                        name='partition{}.pbtxt'.format(i),
+                    )
 
             writer.add_run_metadata(run_metadata, session_tag)
             writer.close()
