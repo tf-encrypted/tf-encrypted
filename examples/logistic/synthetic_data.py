9f972345802215829b6d2f8dd17acc8ffe025bee
jvmancuso <jason@dropoutlabs.com>
2019-05-02 11:48:01 -0400

    simplify data gen, tweak distribution params

--- examples/logistic/synthetic_data.py
+++ examples/logistic/synthetic_data.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+# import pandas as pd
 np.random.seed(42)
 
 
@@ -9,8 +9,8 @@ target_slope = -1.0
 
 y0_mu = [1., 1.]
 y1_mu = [3., 3.]
-yt_sig = [.5, .5]
-yv_sig = [.8, .8]
+yt_sig = [.3, .3]
+yv_sig = [.6, .6]
 
 train_size = 4000
 test_size = 2000
@@ -55,47 +55,13 @@ def softmax(x, axis=None):
     return num / den
 
 
-def sample_alice_data(x_train, y_train, sz):
-    if isinstance(sz, (tuple, list)):
-        sz0, sz1 = sz
-    else:
-        sz0 = sz1 = sz // 2
-
-    x0 = x_train[y_train == 0]
-    x1 = x_train[y_train == 1]
-    ix = np.arange(len(x_train))
-    ix0 = ix[y_train == 0]
-    ix1 = ix[y_train == 1]
-
-    l2_10 = np.exp(np.exp(np.linalg.norm(y1_mu - x0, axis=-1)))
-    l2_10 /= np.max(l2_10)
-    l2_01 = np.exp(np.exp(np.linalg.norm(y0_mu - x1, axis=-1)))
-    l2_01 /= np.max(l2_01)
-
-    p0 = softmax(1 - l2_10)
-    p1 = softmax(1 - l2_01)
-
-    ix0_alice = np.random.choice(ix0, sz0, replace=False, p=p0)
-    ix1_alice = np.random.choice(ix1, sz1, replace=False, p=p1)
-    ix_alice = np.concatenate((ix0_alice, ix1_alice))
-
-    msk = np.zeros(y_train.shape, dtype=bool)
-    msk[ix_alice] = True
-    x_alice, y_alice = x_train[msk], y_train[msk]
-    x_bob, y_bob = x_train[~msk], y_train[~msk]
-
-    return (x_alice, y_alice), (x_bob, y_bob)
-
-
 if __name__ == "__main__":
     tparams = [(y0_mu, yt_sig), (y1_mu, yt_sig)]
     vparams = [(y0_mu, yv_sig), (y1_mu, yv_sig)]
 
-    x_train, y_train = gen_dataset(*tparams, sz=train_size)
+    x_bob, y_bob = gen_dataset(*tparams, sz=train_size)
+    x_alice, y_alice = gen_dataset(*vparams, sz=alice_size)
     x_test, y_test = gen_dataset(*vparams, sz=test_size)
-    (x_alice, y_alice), (x_bob, y_bob) = sample_alice_data(x_train,
-                                                           y_train,
-                                                           alice_size)
 
     y_train = np.expand_dims(y_train, 1)
     y_test = np.expand_dims(y_test, 1)
@@ -103,8 +69,6 @@ if __name__ == "__main__":
     y_bob = np.expand_dims(y_bob, 1)
 
     cols = ["x", "y", "label"]
-    train = pd.DataFrame(np.concatenate((x_train, y_train), axis=-1),
-                         columns=cols).to_csv(outpath + "train")
     test = pd.DataFrame(np.concatenate((x_test, y_test), axis=-1),
                         columns=cols).to_csv(outpath + "test")
     alice = pd.DataFrame(np.concatenate((x_alice, y_alice), axis=-1),
@@ -115,7 +79,7 @@ if __name__ == "__main__":
     if visualize:
         plt.scatter(x=x_bob[:, 0], y=x_bob[:, 1], c=y_bob)
         plt.show()
-        plt.scatter(x=x_train[:, 0], y=x_train[:, 1], c=y_train)
+        plt.scatter(x=x_alice[:, 0], y=x_alice[:, 1], c=y_alice)
         plt.show()
         plt.scatter(x=x_test[:, 0], y=x_test[:, 1], c=y_test)
         plt.show()
