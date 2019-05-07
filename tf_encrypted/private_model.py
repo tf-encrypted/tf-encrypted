de58d919675fe32ebd1aecf07ddba65447e30128
jvmancuso <jason@dropoutlabs.com>
2019-05-06 20:56:28 -0400

    linting setup.py and top-level tfe files

--- tf_encrypted/private_model.py
+++ tf_encrypted/private_model.py
@@ -1,81 +1,85 @@
+"""An abstraction for private models."""
 import tensorflow as tf
-import tf_encrypted as tfe
 
 from tensorflow.keras import backend as K
 from tensorflow.python.framework import graph_util
 from tensorflow.python.platform import gfile
 from tensorflow.python.framework.graph_util_impl import remove_training_nodes
+import tf_encrypted as tfe
 
 
-class PrivateModel():
-    def __init__(self, output_node):
-        self.output_node = output_node
+class PrivateModel:
+  """An implementation of private models."""
+  def __init__(self, output_node):
+    self.output_node = output_node
 
-    # TODO support multiple inputs
-    def private_predict(self, input, input_name=None, tag="prediction"):
-        if input_name is None:
-            name = "private-input/api/0:0"
-        else:
-            name = input_name
+  # TODO support multiple inputs
+  def private_predict(self, x, input_name=None, tag="prediction"):
+    """Perform a private prediction."""
+    if input_name is None:
+      name = "private-input/api/0:0"
+    else:
+      name = input_name
 
-        pl = tf.get_default_graph().get_tensor_by_name(name)
+    pl = tf.get_default_graph().get_tensor_by_name(name)
 
-        with tfe.Session() as sess:
-            sess.run(tf.global_variables_initializer())
+    with tfe.Session() as sess:
+      sess.run(tf.global_variables_initializer())
 
-            output = sess.run(
-                self.output_node.reveal(),
-                feed_dict={pl: input},
-                tag=tag
-            )
+      op = self.output_node.reveal()
+      output = sess.run(op, feed_dict={pl: x}, tag=tag)
 
-            return output
+      return output
 
 
 def load_graph(model_file, model_name=None):
+  """Load a plaintext model from protobuf."""
 
-    input_spec = []
-    with gfile.GFile(model_file, 'rb') as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
+  input_spec = []
+  with gfile.GFile(model_file, 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
 
-        for node in graph_def.node:
-            if node.op != "Placeholder":
-                continue
+    for node in graph_def.node:
+      if node.op != "Placeholder":
+        continue
 
-            input_spec.append({
-                'name': node.name,
-                'dtype': node.attr['dtype'].type,
-                'shape': [1] + [int(d.size) for d in node.attr['shape'].shape.dim[1:]]
-            })
+      input_spec.append({
+          'name': node.name,
+          'dtype': node.attr['dtype'].type,
+          'shape': [1] + [int(d.size) for d in node.attr['shape'].shape.dim[1:]]
+      })
 
-    inputs = []
-    for i, spec in enumerate(input_spec):
-        def scope(i, spec):
-            def provide_input() -> tf.Tensor:
-                if model_name is None:
-                    name = "api/{}".format(i)
-                else:
-                    name = "api/{}/{}".format(model_name, i)
+  inputs = []
+  for i, spec in enumerate(input_spec):
+    def scope(i, spec):
+      def provide_input() -> tf.Tensor:
+        if model_name is None:
+          name = "api/{}".format(i)
+        else:
+          name = "api/{}/{}".format(model_name, i)
 
-                pl = tf.placeholder(tf.float32, shape=spec['shape'], name=name)
-                return pl
+        pl = tf.placeholder(tf.float32, shape=spec['shape'], name=name)
+        return pl
 
-            return provide_input
+      return provide_input
 
-        inputs.append(scope(i, spec))
+    inputs.append(scope(i, spec))
 
-    return graph_def, inputs
+  return graph_def, inputs
 
 
 def secure_model(model):
-    session = K.get_session()
-    min_graph = graph_util.convert_variables_to_constants(session, session.graph_def, [node.op.name for node in model.outputs])
-    tf.train.write_graph(min_graph, '/tmp', 'model.pb', as_text=False)
+  """Secure a plaintext model from the current session."""
+  session = K.get_session()
+  min_graph = graph_util.convert_variables_to_constants(
+      session, session.graph_def, [node.op.name for node in model.outputs])
+  tf.train.write_graph(min_graph, '/tmp', 'model.pb', as_text=False)
 
-    graph_def, inputs = load_graph('/tmp/model.pb')
+  graph_def, inputs = load_graph('/tmp/model.pb')
 
-    c = tfe.convert.convert.Converter()
-    y = c.convert(remove_training_nodes(graph_def), tfe.convert.registry(), 'input-provider', inputs)
+  c = tfe.convert.convert.Converter()
+  y = c.convert(remove_training_nodes(graph_def),
+                tfe.convert.registry(), 'input-provider', inputs)
 
-    return PrivateModel(y)
+  return PrivateModel(y)
--- tf_encrypted/session.py
+++ tf_encrypted/session.py
@@ -1,9 +1,8 @@
-from typing import Dict, List, Optional, Any, Union
-from collections import defaultdict
-import logging
+"""TF Encrypted extension of tf.Session."""
 import os
+from typing import List, Any, Union
+from collections import defaultdict
 
-import numpy as np
 import tensorflow as tf
 from tensorflow.python.client import timeline
 from tensorflow.python import debug as tf_debug
@@ -13,199 +12,173 @@ from .protocol.pond import PondPublicTensor
 from .tensor.factory import AbstractTensor
 
 
-__TFE_STATS__ = bool(os.getenv('TFE_STATS', False))
-__TFE_TRACE__ = bool(os.getenv('TFE_TRACE', False))
-__TFE_DEBUG__ = bool(os.getenv('TFE_DEBUG', False))
-__TENSORBOARD_DIR__ = str(os.getenv('TFE_STATS_DIR', '/tmp/tensorboard'))
+__tfe_events__ = bool(os.getenv('TFE_EVENTS', ""))
+__tfe_trace__ = bool(os.getenv('TFE_TRACE', ""))
+__tfe_debug__ = bool(os.getenv('TFE_DEBUG', ""))
+__tensorboard_dir__ = str(os.getenv('TFE_EVENTS_DIR', '/tmp/tensorboard'))
 
 _run_counter = defaultdict(int)  # type: Any
 
-logging.basicConfig()
-logger = logging.getLogger('tf_encrypted')
-logger.setLevel(logging.DEBUG)
-
 
 class Session(tf.Session):
-    """
-    Wrap a Tensorflow Session.
+  """
+  Wrap a Tensorflow Session.
 
-    See the documentation of
-    `tf.Session <https://www.tensorflow.org/api_docs/python/tf/Session>`_
+  See the documentation of
+  `tf.Session <https://www.tensorflow.org/api_docs/python/tf/Session>`_
+  for more details.
+
+  :param Optional[tf.Graph] graph: A :class:`tf.Graph`.  Used similarly.
+    This is the graph to be launched.  If nothing is specified, then the
+    default graph in the session will be used.
+  :param Optional[~tensorflow_encrypted.config.Config] config:  A
+    :class:`Local <tf_encrypted.config.LocalConfig/>` or
+    :class:`Remote <tf_encrypted.config.RemoteConfig>` config to be supplied
+    when executing the graph.
+  """
+  def __init__(self, graph=None, config=None, target=None):
+    if config is None:
+      config = get_config()
+
+    default_target, config_proto = config.get_tf_config()
+    if target is None:
+      target = default_target
+
+    if isinstance(config, RemoteConfig):
+      print("Starting session on target '{}' using config {}".format(
+          target, config_proto))
+    # print(self._closed)
+    super(Session, self).__init__(target, graph, config_proto)
+    print("closed", self._closed)
+    # self.sess = tf.Session(target, graph, config_proto)
+
+    global __tfe_debug__
+    print("tfedebug", __tfe_debug__)
+
+    if __tfe_debug__:
+    # if False:
+      print('Session in debug mode')
+      self = tf_debug.LocalCLIDebugWrapperSession(self)  # pylint: disable=self-cls-assignment
+
+  def _sanitize_fetches(self, fetches) -> Union[List, tf.Tensor, tf.Operation]:
+    """
+    Sanitize `fetches` supplied to tfe.Session.run into
+    tf.Session.run-compatible `fetches`.
+    """
+
+    if isinstance(fetches, (list, tuple)):
+      return [self._sanitize_fetches(fetch) for fetch in fetches]
+    if isinstance(fetches, (tf.Tensor, tf.Operation)):
+      return fetches
+    if isinstance(fetches, PondPublicTensor):
+      return fetches.decode()
+    if isinstance(fetches, AbstractTensor):
+      return fetches.to_native()
+    raise TypeError("Don't know how to fetch {}".format(type(fetches)))
+
+  def run(self, fetches, feed_dict=None, tag=None, write_trace=False):
+    # pylint: disable=arguments-differ
+    """
+    run(fetches, feed_dict, tag, write_trace) -> Any
+
+    See the documentation for
+    `tf.Session.run <https://www.tensorflow.org/api_docs/python/tf/Session#run>`_
     for more details.
 
-    :param Optional[tf.Graph] graph: A :class:`tf.Graph`.  Used in the same as in tensorflow.
-            This is the graph to be launched.  If nothing is specified then the default session
-            graph will be used.
-    :param Optional[~tf_encrypted.config.Config] config:  A
-            :class:`Local <tf_encrypted.config.LocalConfig/>` or
-            :class:`Remote <tf_encrypted.config.RemoteConfig>` config to be used to
-            execute the graph.
+    This method functions just as the one from tensorflow.
+
+    The value returned by run() has the same shape as the fetches argument,
+    where the leaves are replaced by the corresponding values returned by
+    TensorFlow.
+
+    :param Any fetches: A single graph element, a list of graph elements, or a
+      dictionary whose values are graph elements or lists of graph elements
+      (described in tf.Session.run docs).
+    :param str->np.ndarray feed_dict: A dictionary that maps graph elements to
+      values (described in tf.Session.run docs).
+    :param str tag: An optional namespace to run the session under.
+    :param bool write_trace: If true, the session logs will be dumped for use
+      in Tensorboard.
     """
 
-    def __init__(
-        self,
-        graph=None,
-        config=None,
-        target=None,
-        **tf_config_kwargs
-    ) -> None:
-        if config is None:
-            config = get_config()
+    sanitized_fetches = self._sanitize_fetches(fetches)
 
-        default_target, configProto = config.get_tf_config(**tf_config_kwargs)
-        if target is None:
-            target = default_target
+    if not __tfe_events__ or tag is None:
+      fetches_out = super(Session, self).run(
+          sanitized_fetches,
+          feed_dict=feed_dict
+      )
+    else:
+      session_tag = "{}{}".format(tag, _run_counter[tag])
+      run_tag = os.path.join(__tensorboard_dir__, session_tag)
+      _run_counter[tag] += 1
 
-        if isinstance(config, RemoteConfig):
-            logger.info("Starting session on target '{}' using config {}".format(
-                        target, configProto))
+      writer = tf.summary.FileWriter(run_tag, self.graph)
+      run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+      run_metadata = tf.RunMetadata()
 
-        super(Session, self).__init__(target, graph, configProto)
-        # self.sess = tf.Session(target, graph, configProto)
+      fetches_out = super(Session, self).run(
+          sanitized_fetches,
+          feed_dict=feed_dict,
+          options=run_options,
+          run_metadata=run_metadata
+      )
 
-        global __TFE_DEBUG__
-        if __TFE_DEBUG__:
-            logger.info("Session in debug mode")
-            self = tf_debug.LocalCLIDebugWrapperSession(self)
+      writer.add_run_metadata(run_metadata, session_tag)
+      writer.close()
 
-    def _sanitize_fetches(self, fetches: Any) -> Union[List[Any], tf.Tensor, tf.Operation]:
+      if __tfe_trace__ or write_trace:
+        tracer = timeline.Timeline(run_metadata.step_stats)
+        chrome_trace = tracer.generate_chrome_trace_format()
+        trace_fname = '{}/{}.ctr'.format(__tensorboard_dir__, session_tag)
+        with open(trace_fname, 'w') as f:
+          f.write(chrome_trace)
 
-        if isinstance(fetches, (list, tuple)):
-            return [self._sanitize_fetches(fetch) for fetch in fetches]
-        else:
-            if isinstance(fetches, (tf.Tensor, tf.Operation)):
-                return fetches
-            elif isinstance(fetches, PondPublicTensor):
-                return fetches.decode()
-            elif isinstance(fetches, AbstractTensor):
-                return fetches.to_native()
-            else:
-                raise TypeError("Don't know how to fetch {}", type(fetches))
-
-    def run(
-        self,
-        fetches: Any,
-        feed_dict: Dict[tf.Tensor, np.ndarray] = {},
-        tag: Optional[str] = None,
-        write_trace: bool = False,
-        output_partition_graphs: bool = False,
-    ):
-        """
-        run(fetches, feed_dict, tag, write_trace) -> Any
-
-        See the documentation for
-        `tf.Session.run <https://www.tensorflow.org/api_docs/python/tf/Session#run>`_
-        for more details.
-
-        This method functions just as the one from tensorflow.
-
-        The value returned by run() has the same shape as the fetches argument, where the leaves
-        are replaced by the corresponding values returned by TensorFlow.
-
-        :param Any fetches: A single graph element, a list of graph elements, or a dictionary whose
-                            values are graph elements or lists of graph elements
-                            (described in tf.Session.run docs).
-        :param str->np.ndarray feed_dict: A dictionary that maps graph elements to values
-                                          (described in tf.Session.run docs).
-        :param str tag: An optional namespace to run the session under.
-        :param bool write_trace: If true, the session logs will be dumped for use in Tensorboard.
-        """
-
-        sanitized_fetches = self._sanitize_fetches(fetches)
-
-        if not __TFE_STATS__ or tag is None:
-            fetches_out = super(Session, self).run(
-                sanitized_fetches,
-                feed_dict=feed_dict
-            )
-        else:
-            session_tag = "{}{}".format(tag, _run_counter[tag])
-            run_tag = os.path.join(__TENSORBOARD_DIR__, session_tag)
-            _run_counter[tag] += 1
-
-            writer = tf.summary.FileWriter(run_tag, self.graph)
-            run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE,
-                                        output_partition_graphs=output_partition_graphs)
-            run_metadata = tf.RunMetadata()
-
-            fetches_out = super(Session, self).run(
-                sanitized_fetches,
-                feed_dict=feed_dict,
-                options=run_options,
-                run_metadata=run_metadata
-            )
-
-            if output_partition_graphs:
-                for i, g in enumerate(run_metadata.partition_graphs):
-                    tf.io.write_graph(
-                        g,
-                        logdir=os.path.join(__TENSORBOARD_DIR__, session_tag),
-                        name='partition{}.pbtxt'.format(i),
-                    )
-
-            writer.add_run_metadata(run_metadata, session_tag)
-            writer.close()
-
-            if __TFE_TRACE__ or write_trace:
-                tracer = timeline.Timeline(run_metadata.step_stats)
-                chrome_trace = tracer.generate_chrome_trace_format()
-                with open('{}/{}.ctr'.format(__TENSORBOARD_DIR__, session_tag), 'w') as f:
-                    f.write(chrome_trace)
-
-        return fetches_out
+    return fetches_out
 
 
-def setMonitorStatsFlag(monitor_stats: bool = False) -> None:
-    """
-    setMonitorStatsFlag(monitor_stats)
+def set_tfe_events_flag(monitor_events: bool = False) -> None:
+  """
+  set_tfe_events_flag(monitor_events)
 
-    Set flag to enable or disable monitoring of runtime statistics for each call to session.run().
+  Set flag to enable or disable monitoring of runtime statistics for each call
+  to session.run().
 
-    :param bool monitor_stats: Enable or disable stats, disabled by default.
-    """
-    global __TFE_STATS__
-    if monitor_stats is True:
-        logger.info("Writing event files for every session.run() call with a tag")
+  :param bool monitor_events: Enable or disable stats, disabled by default.
+  """
+  global __tfe_events__
+  if monitor_events is True:
+    print(("Tensorflow encrypted is monitoring statistics for each",
+           "session.run() call using a tag"))
 
-    __TFE_STATS__ = monitor_stats
-
-
-def setTFEDebugFlag(debug: bool = False) -> None:
-    """
-    setTFEDebugFlag(debug)
-
-    Set flag to enable or disable debugging mode for TF Encrypted.
-
-    :param bool debug: Enable or disable debugging, disabled by default.
-    """
-    global __TFE_DEBUG__
-    if debug is True:
-        logger.info("Running in DEBUG mode")
-
-    __TFE_DEBUG__ = debug
+  __tfe_events__ = monitor_events
 
 
-def setTFETraceFlag(trace: bool = False) -> None:
-    """
-    setTFETraceFlag(trace)
+def set_tfe_debug_flag(debug: bool = False) -> None:
+  """
+  set_tfe_debug_flag(debug)
 
-    Set flag to enable or disable tracing in TF Encrypted.
+  Set flag to enable or disable debugging mode for TF Encrypted.
 
-    :param bool trace: Enable or disable tracing, disabled by default.
-    """
-    global __TFE_TRACE__
-    if trace is True:
-        logger.info("Writing trace files for every session.run() call with a tag")
+  :param bool debug: Enable or disable debugging, disabled by default.
+  """
+  global __tfe_debug__
+  if debug is True:
+    print("Tensorflow encrypted is running in DEBUG mode")
 
-    __TFE_TRACE__ = trace
+  __tfe_debug__ = debug
 
 
-def setLogDirectory(path):
-    """
-    Sets the directory to write TensorBoard event and trace files to.
-    """
-    global __TENSORBOARD_DIR__
-    if path:
-        logger.info("Writing event and trace files to '{}'".format(path))
+def set_tfe_trace_flag(trace: bool = False) -> None:
+  """
+  set_tfe_trace_flag(trace)
 
-    __TENSORBOARD_DIR__ = path
+  Set flag to enable or disable tracing in TF Encrypted.
+
+  :param bool trace: Enable or disable tracing, disabled by default.
+  """
+  global __tfe_trace__
+  if trace is True:
+    print("Tensorflow encrypted is dumping computation traces")
+
+  __tfe_trace__ = trace
