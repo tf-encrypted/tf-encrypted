24621b9489d7b69a8fb3596d73fb6dafea59fd7f
jvmancuso <jason@dropoutlabs.com>
2019-05-06 20:57:35 -0400

    linting player/

--- tf_encrypted/player/player.py
+++ tf_encrypted/player/player.py
@@ -1,28 +1,29 @@
-from typing import Optional
-
+"""Implementation of the Player abstraction."""
 import tensorflow as tf
 
 
-class Player(object):
-    """
-    An abstraction for players in the game-theoretic model of a secure computation.
+class Player:
+  """
+  An abstraction for players in the game-theoretic threat model of
+  a secure computation.
 
-    :param str name: Name of the player
-    :param int index: Index of the player (for ordering)
-    :param str device_name: Name of device (fully expanded)
-    :param str host: IP/domain address of the player's device, defaults to None
-    """
-    def __init__(self, name: str, index: int, device_name: str, host: Optional[str] = None) -> None:
-        self.name = name
-        self.index = index
-        self.device_name = device_name
-        self.host = host
+  :param str name: Name of the player
+  :param int index: Index of the player (for ordering)
+  :param str device_name: Name of device (fully expanded)
+  :param str host: IP/domain address of the player's device, defaults to None
+  """
+
+  def __init__(self, name, index, device_name, host=None):
+    self.name = name
+    self.index = index
+    self.device_name = device_name
+    self.host = host
 
 
-def player(player: Player):
-    """
-    Retrieves the tf.device associated with a :class:`Player` object.
+def player_device(player: Player):
+  """
+  Retrieves the tf.device associated with a :class:`Player` object.
 
-    :param Player player: The :class:`Player` object.
-    """
-    return tf.device(player.device_name)
+  :param Player player: The :class:`Player` object.
+  """
+  return tf.device(player.device_name)
