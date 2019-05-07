b4182dbd1faf28140ce2558c2627bea15d9a5c93
jvmancuso <jason@dropoutlabs.com>
2019-05-06 20:57:20 -0400

    linting layers/

--- tf_encrypted/layers/activation.py
+++ tf_encrypted/layers/activation.py
@@ -1,91 +1,89 @@
+# pylint: disable=arguments-differ
+"""Various activation functions implemented as Layer objects."""
 from typing import List
 
-from . import core
+from tf_encrypted.layers.core import Layer
+
+backward_msg = "`backward` is not implemented for layer {}"
 
 
-class Sigmoid(core.Layer):
+class Sigmoid(Layer):
+  """
+  Sigmoid Layer
 
+  :See: tf.nn.Sigmoid
+  """
+  def get_output_shape(self) -> List[int]:
+    return self.input_shape
+
+  def initialize(self):
+    pass
+
+  def forward(self, x):
+    y = self.prot.sigmoid(x)
+    self.layer_output = y
+    return y
+
+  def backward(self, d_y):
+    y = self.layer_output
+    d_x = d_y * y * (y.neg() + 1)
+    return d_x
+
+
+class Relu(Layer):
+  """
+  Relu Layer
+
+  :See: tf.nn.relu
+  """
+  def get_output_shape(self) -> List[int]:
+    return self.input_shape
+
+  def initialize(self, *args, **kwargs) -> None:
+    pass
+
+  def forward(self, x):
     """
-    Sigmoid Layer
-
-    :See: tf.nn.Sigmoid
+    :param ~tensorflow_encrypted.protocol.pond.PondTensor x: The input tensor
+    :rtype: ~tensorflow_encrypted.protocol.pond.PondTensor
+    :returns: A pond tensor with the same backing type as the input tensor.
     """
+    y = self.prot.relu(x)
+    self.layer_output = y
+    return y
 
-    def get_output_shape(self) -> List[int]:
-        return self.input_shape
-
-    def initialize(self, *args, **kwargs) -> None:
-        pass
-
-    def forward(self, x):
-        y = self.prot.sigmoid(x)
-        self.layer_output = y
-        return y
-
-    def backward(self, d_y, *args):
-        y = self.layer_output
-        d_x = d_y * y * (y.neg() + 1)
-        return d_x
-
-
-class Relu(core.Layer):
-
+  # TODO Approximate Relu derivate to implement backward
+  def backward(self, d_y, *args):
     """
-    Relu Layer
+    `backward` is not implemented for `Relu`
 
-    :See: tf.nn.relu
+    :raises: NotImplementedError
     """
-
-    def get_output_shape(self) -> List[int]:
-        return self.input_shape
-
-    def initialize(self, *args, **kwargs) -> None:
-        pass
-
-    def forward(self, x):
-        """
-        :param ~tensorflow_encrypted.protocol.pond.PondTensor x: The input tensor
-        :rtype: ~tensorflow_encrypted.protocol.pond.PondTensor
-        :returns: A pond tensor with the same backing type as the input tensor.
-        """
-        y = self.prot.relu(x)
-        self.layer_output = y
-        return y
-
-    # TODO Approximate Relu derivate to implement backward
-    def backward(self, d_y, *args):
-        """
-        `backward` is not implemented for `Relu`
-
-        :raises: NotImplementedError
-        """
-        raise NotImplementedError
+    raise NotImplementedError(backward_msg.format("Relu"))
 
 
-class Tanh(core.Layer):
+class Tanh(Layer):
+  """
+  Tanh Layer
 
+  :See: tf.nn.tanh
+  """
+  def get_output_shape(self) -> List[int]:
+    return self.input_shape
+
+  def initialize(self, *args, **kwargs) -> None:
+    pass
+
+  def forward(self, x):
+    y = self.prot.tanh(x)
+    self.layer_output = y
+    return y
+
+  # TODO Approximate Relu derivate to implement backward
+  def backward(self, d_y, *args):
     """
-    Tanh Layer
+    `backward` is not implemented for `Tanh`
 
-    :See: tf.nn.tanh
+    :raises: NotImplementedError
     """
-
-    def get_output_shape(self) -> List[int]:
-        return self.input_shape
-
-    def initialize(self, *args, **kwargs) -> None:
-        pass
-
-    def forward(self, x):
-        y = self.prot.tanh(x)
-        self.layer_output = y
-        return y
-
-    # TODO Approximate Relu derivate to implement backward
-    def backward(self, d_y, *args):
-        """
-        `backward` is not implemented for `Tanh`
-
-        :raises: NotImplementedError
-        """
-        raise NotImplementedError
+    raise NotImplementedError(backward_msg.format("Tanh"))
--- tf_encrypted/layers/core.py
+++ tf_encrypted/layers/core.py
@@ -1,55 +1,40 @@
-from typing import Optional, List, Any, Union
-import tensorflow as tf
-import numpy as np
+"""Includes base classes used by all layer types."""
+
+from typing import List, Optional
 from abc import ABC, abstractmethod
-from ..protocol.protocol import get_protocol, Protocol
-from ..protocol.pond import PondPublicTensor, PondPrivateTensor, TFEVariable
+
+from tf_encrypted.protocol.pond import TFEVariable
+from tf_encrypted.protocol.protocol import get_protocol, Protocol
 
 # TODO
 # Split backward function in compute_gradient and compute_backpropagated_error?
 
 
 class Layer(ABC):
+  """
+  Base class for all layers.
+  """
+  def __init__(self, input_shape: List[int]) -> None:
+    self.input_shape = input_shape
+    self.output_shape = self.get_output_shape()
+    self.layer_output = None
 
-    def __init__(self, input_shape: List[int]) -> None:
-        self.input_shape = input_shape
-        self.output_shape = self.get_output_shape()
-        self.layer_output = None
+  @abstractmethod
+  def get_output_shape(self) -> List[int]:
+    """Returns the layer's output shape"""
 
-    @abstractmethod
-    def get_output_shape(self) -> List[int]:
-        """Returns the layer's output shape"""
+  @abstractmethod
+  def initialize(self, *args, **kwargs) -> None:
+    """Initialize any necessary tensors."""
 
-    @abstractmethod
-    def initialize(
-        self,
-        *args: Optional[Union[np.ndarray, tf.Tensor, PondPublicTensor, PondPrivateTensor]],
-        **kwargs: Optional[Union[np.ndarray, tf.Tensor, PondPublicTensor, PondPrivateTensor]]
-    ) -> None:
-        pass
+  @abstractmethod
+  def forward(self, *args, **kwargs) -> Optional[TFEVariable]:
+    """Forward pass for inference"""
 
-    @abstractmethod
-    def forward(
-        self,
-        *args: Any,
-        **kwargs: Any
-    ) -> Optional[TFEVariable]:
-        """
-        Forward pass for inference
-        """
-        pass
+  # TODO[jason]: @abstractmethod
+  def backward(self, *args, **kwargs) -> Optional[TFEVariable]:
+    """Backward pass for training."""
 
-    @abstractmethod
-    def backward(
-        self,
-        *args: Any,
-        **kwargs: Any
-    ) -> Optional[TFEVariable]:
-        """
-        The backward pass for training.
-        """
-        pass
-
-    @property
-    def prot(self) -> Optional[Protocol]:
-        return get_protocol()
+  @property
+  def prot(self) -> Optional[Protocol]:
+    return get_protocol()
