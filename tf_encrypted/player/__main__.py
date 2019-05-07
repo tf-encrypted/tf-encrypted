89cdd7d4dc66010895623760b0914d1bc04222f0
jvmancuso <jason@dropoutlabs.com>
2019-05-06 20:57:35 -0400

    linting player/

--- tf_encrypted/player/__main__.py
+++ tf_encrypted/player/__main__.py
@@ -1,16 +1,18 @@
-from ..config import RemoteConfig
+"""Executable for hosting a Player"""
+from tf_encrypted.config import RemoteConfig
 
 if __name__ == '__main__':
 
-    import argparse
+  import argparse
 
-    parser = argparse.ArgumentParser(description="Run a TF Encrypted player")
-    parser.add_argument('name', metavar='NAME', type=str,
-                        help='name of player as specified in the config file')
-    parser.add_argument('--config', metavar='FILE', type=str,
-                        help='path to configuration file', default='./config.json')
-    args = parser.parse_args()
+  parser = argparse.ArgumentParser(description="Run a TF Encrypted player")
+  parser.add_argument('name', metavar='NAME', type=str,
+                      help='name of player as specified in the config file')
+  parser.add_argument('--config', metavar='FILE', type=str,
+                      default='./config.json',
+                      help='path to configuration file')
+  args = parser.parse_args()
 
-    config = RemoteConfig.load(args.config)
-    server = config.server(args.name, start=True)
-    server.join()
+  config = RemoteConfig.load(args.config)
+  server = config.server(args.name, start=True)
+  server.join()
