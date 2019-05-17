0cfade099f7c3b5a2f78695bb1ff628f9fb87202
yann dupis <yann.dupis@gmail.com>
2019-05-16 17:46:52 -0700

    fix doc

--- tf_encrypted/keras/layers/activation_layer.py
+++ tf_encrypted/keras/layers/activation_layer.py
@@ -5,7 +5,7 @@ from tf_encrypted.keras import activations
 class Activation(Layer):
   """Applies an activation function to an output.
   Arguments:
-      activation: name of activation function to use
+      activation: name of activation function to use or
           TF Encrypted operation.
   Input shape:
       Arbitrary. Use the keyword argument `input_shape`
