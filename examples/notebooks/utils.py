b900495aa8e14f110c4379170efcb20162f3ff1a
Morten Dahl <mortendahlcs@gmail.com>
2019-05-22 13:56:21 +0200

    Notebook for FIFO queues

---
+++ examples/notebooks/utils.py
@@ -0,0 +1,9 @@
+"""
+Various helpers that make using TensorFlow and TF Encrypted in notebooks
+easier.
+"""
+
+import tensorflow as tf
+
+def print_in_notebook(x):
+  return tf.py_func(print, [x], Tout=[])
