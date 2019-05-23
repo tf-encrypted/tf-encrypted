041896542b28a445a0fe263d1b1223fc1f57bf06
Yann Dupis <yann.dupis@gmail.com>
2019-05-23 09:48:15 -0700

    Add Flatten layer (#494)
    * add test
    
    * add channel first support
    
    * fix compute output shape
    
    * reduce precision to reduce risk of 1.25% mismatch
    
    * set global agreement treshold to rtol=1e-2

--- tf_encrypted/keras/layers/__init__.py
+++ tf_encrypted/keras/layers/__init__.py
@@ -4,10 +4,12 @@ from __future__ import absolute_import
 from .dense import Dense
 from .activation import Activation
 from .convolutional import Conv2D
+from .flatten import Flatten
 
 
 __all__ = [
     'Dense',
     'Activation',
     'Conv2D',
+    'Flatten'
 ]
---
+++ tf_encrypted/keras/layers/flatten_test.py
@@ -0,0 +1,37 @@
+# pylint: disable=missing-docstring
+import unittest
+
+import numpy as np
+import tensorflow as tf
+
+import tf_encrypted as tfe
+from tf_encrypted.keras.testing_utils import agreement_test
+
+np.random.seed(42)
+
+
+class TestFlatten(unittest.TestCase):
+
+  def setUp(self):
+    tf.reset_default_graph()
+
+  def test_flatten_rank_four(self):
+    self._core_flatten(input_shape=[4, 5, 2, 2])
+
+  def test_flatten_rank_one(self):
+    self._core_flatten(input_shape=[4])
+
+  def test_flatten_channels_first(self):
+    self._core_flatten(input_shape=[4, 5, 2, 2],
+                       data_format='channels_first')
+
+  def _core_flatten(self, **layer_kwargs):
+    input_shape = layer_kwargs['input_shape']
+
+    agreement_test(tfe.keras.layers.Flatten,
+                   kwargs=layer_kwargs,
+                   input_shape=input_shape)
+
+
+if __name__ == '__main__':
+  unittest.main()
