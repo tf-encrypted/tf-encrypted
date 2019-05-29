9d0319f9c36158ce2b042396a3e4ed228cba770e
Morten Dahl <mortendahlcs@gmail.com>
2019-05-29 15:57:30 +0200

    Release 0.5.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'The TF Encrypted Authors'
 # The short X.Y version
 version = '0.5.4'
 # The full version, including alpha/beta/rc tags
-release = '0.5.4-rc0'
+release = '0.5.4'
 
 
 # -- General configuration ---------------------------------------------------
