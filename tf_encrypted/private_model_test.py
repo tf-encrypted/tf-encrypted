d20f3a7eb51c31e0e0b50953f21b5d0f2c8cbe48
Morten Dahl <mortendahlcs@gmail.com>
2019-06-06 18:29:14 +0200

    merge test files

--- tf_encrypted/private_model_test.py
+++ tf_encrypted/private_model_test.py
@@ -3,11 +3,13 @@ import unittest
 
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras import backend as K
+
 import tf_encrypted as tfe
 from tf_encrypted.private_model import PrivateModel
-
+from tf_encrypted.private_model import secure_model
 # TODO this is a bit weird
-from .convert.convert_test import read_graph, export_matmul
+from tf_encrypted.convert.convert_test import read_graph, export_matmul
 
 
 class TestPrivateModel(unittest.TestCase):
@@ -29,6 +31,32 @@ class TestPrivateModel(unittest.TestCase):
       output = model.private_predict(np.ones([1, 2]))
 
     np.testing.assert_array_equal(output, [[2.]])
+
+
+class TestSecureModel(unittest.TestCase):
+
+  def setUp(self):
+    K.clear_session()
+
+  def tearDown(self):
+    K.clear_session()
+
+  def test_secure_model(self):
+    with tfe.protocol.Pond():
+      tf.random.set_random_seed(42)
+
+      d = tf.keras.layers.Dense(1, input_shape=(10,), use_bias=False)
+      model = tf.keras.Sequential([
+          d
+      ])
+
+      input = np.ones((1, 10))
+      output = model.predict(input)
+
+      s_model = secure_model(model)
+      s_output = s_model.private_predict(input)
+
+      np.testing.assert_array_almost_equal(s_output, output, 4)
 
 
 if __name__ == '__main__':
--- tf_encrypted/protocol/pond/pond_test.py
+++ tf_encrypted/protocol/pond/pond_test.py
@@ -6,6 +6,56 @@ import tensorflow as tf
 
 import tf_encrypted as tfe
 from tf_encrypted.protocol.pond import PondPublicTensor
+from tf_encrypted.tensor import int64factory, int100factory, native_factory
+from tf_encrypted.tensor import fixed100, fixed100_ni
+
+
+class TestTruncate(unittest.TestCase):
+  def setUp(self):
+    tf.reset_default_graph()
+
+  def test_interactive_truncate(self):
+
+    prot = tfe.protocol.Pond(
+        tensor_factory=int100factory,
+        fixedpoint_config=fixed100,
+    )
+
+    # TODO[Morten] remove this condition
+    if prot.tensor_factory not in [tfe.tensor.int64factory]:
+
+      expected = np.array([12345.6789])
+
+      w = prot.define_private_variable(
+          expected * prot.fixedpoint_config.scaling_factor)  # double precision
+      v = prot.truncate(w)  # single precision
+
+      with tfe.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        actual = sess.run(v.reveal())
+
+      np.testing.assert_allclose(actual, expected)
+
+  def test_noninteractive_truncate(self):
+
+    prot = tfe.protocol.Pond(
+        tensor_factory=int100factory,
+        fixedpoint_config=fixed100_ni,
+    )
+
+    with tfe.Session() as sess:
+
+      expected = np.array([12345.6789])
+
+      w = prot.define_private_variable(
+          expected * prot.fixedpoint_config.scaling_factor)  # double precision
+      v = prot.truncate(w)  # single precision
+
+      sess.run(tf.global_variables_initializer())
+      actual = sess.run(v.reveal())
+
+      np.testing.assert_allclose(actual, expected)
+
 
 
 class TestPondPublicEqual(unittest.TestCase):
@@ -27,6 +77,34 @@ class TestPondPublicEqual(unittest.TestCase):
         answer = sess.run(res)
 
       assert np.array_equal(answer, expected)
+
+
+class TestShare(unittest.TestCase):
+
+  def setUp(self):
+    tf.reset_default_graph()
+
+  def _core_test_sharing(self, dtype):
+
+    expected = np.array([[1, 2, 3], [4, 5, 6]])
+
+    with tfe.protocol.Pond() as prot:
+
+      with tfe.Session() as sess:
+        shares = prot._share(dtype.tensor(expected))
+        actual = sess.run(prot._reconstruct(*shares).to_native())
+
+    np.testing.assert_array_equal(actual, expected)
+
+  def test_int64(self):
+    self._core_test_sharing(int64factory)
+
+  def test_int100(self):
+    self._core_test_sharing(int100factory)
+
+  def test_prime(self):
+    self._core_test_sharing(native_factory(tf.int32, 67))
+
 
 
 if __name__ == '__main__':
