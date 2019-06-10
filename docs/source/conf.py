a751bc4dd261fd14ba860952b9ddda4c84e3e063
jvmancuso <jason@dropoutlabs.com>
2019-06-10 15:30:24 -0400

    Release 0.5.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, The TF Encrypted Authors'
 author = 'The TF Encrypted Authors'
 
 # The short X.Y version
-version = '0.5.4'
+version = '0.5.5'
 # The full version, including alpha/beta/rc tags
-release = '0.5.4'
+release = '0.5.5'
 
 
 # -- General configuration ---------------------------------------------------
