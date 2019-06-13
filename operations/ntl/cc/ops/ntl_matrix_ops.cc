c688b0b6c429f9e98b91babf6da5bd49b3c501f1
Justin Patriquin <justinpatriquin@gmail.com>
2019-06-13 12:49:59 -0300

    forgotten file

---
+++ operations/ntl/cc/ops/ntl_matrix_ops.cc
@@ -0,0 +1,22 @@
+#include "tensorflow/core/framework/op.h"
+
+using namespace tensorflow;
+
+REGISTER_OP("CreateNTLMatrix")
+    .Attr("T: {int32, int64, string}")
+    .Input("value: T")
+    .Input("modulus: int64")
+    .Output("ntl: variant")
+    .SetIsStateful();
+
+REGISTER_OP("MatMulNTL")
+    .Input("val1: variant")
+    .Input("val2: variant")
+    .Output("res: variant")
+    .SetIsStateful();
+
+REGISTER_OP("NTLToNative")
+    .Attr("T: {int32, int64, string}")
+    .Input("ntl: variant")
+    .Output("native: T")
+    .SetIsStateful();
