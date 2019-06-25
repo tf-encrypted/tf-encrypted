5121a436b4b111f4c98673a4401a55a5286f1fed
yann dupis <yann.dupis@gmail.com>
2019-06-24 22:47:47 -0700

    Add test with convolutional model

--- tf_encrypted/keras/layers/layers_utils.py
+++ tf_encrypted/keras/layers/layers_utils.py
@@ -4,22 +4,36 @@ import inspect
 import tensorflow as tf
 
 
+class LayerArgNotImplementedError(NotImplementedError):
+  """Raise error when layer argument is not yet supported in TFE
+  """
+  def __init__(self, arg_name, tf_layer_name, tf_default_arg):
+    super(LayerArgNotImplementedError, self).__init__()
+    self.arg_name = arg_name
+    self.tf_layer_name = tf_layer_name
+    self.tf_default_arg = tf_default_arg
+
+  def __str__(self):
+    arg_not_impl_msg = ("`{}` argument is not implemented for layer {}. "
+                        "Please use the default value of {}.")
+    return arg_not_impl_msg.format(self.arg_name,
+                                   self.tf_layer_name,
+                                   self.tf_default_arg)
+
+
 def not_implemented_arg_err(arg, arg_name, tf_layer_name):
   """Raise error when layer argument is not yet supported in TFE
-    Arguments:
+
+    Args:
       arg: TFE layer argument
       arg_name: TFE layer argument name (string)
       tf_layer_name: Tensorflow keras layer name (string)
-  """
-  arg_not_impl_msg = ("`{}` argument is not implemented for layer {}. "
-                      "Please use {} as a default value")
 
+    Raises:
+      NotImplementedError: if this argument is not implemented for this `layer`.
+  """
   tf_layer_cls = getattr(tf.keras.layers, tf_layer_name)
   layer_sign = inspect.signature(tf_layer_cls.__init__).parameters
   tf_default_arg = layer_sign[arg_name].default
-
   if arg != tf_default_arg:
-    raise NotImplementedError(
-        arg_not_impl_msg.format(arg_name, tf_layer_name, tf_default_arg)
-    )
-  
+    raise NotImplementedError(arg_name, tf_layer_name, tf_default_arg)
