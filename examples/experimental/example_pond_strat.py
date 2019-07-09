d3d7e1be0ada6db77002cbd6be8f11b4f7f6d9dc
jvmancuso <jason@dropoutlabs.com>
2019-07-09 23:39:07 +0200

    <>

--- examples/experimental/example_pond_strat.py
+++ examples/experimental/example_pond_strat.py
@@ -3,6 +3,12 @@ from . import tf_encrypted as tfe
 
 strategy = tfe.protocol.Pond()
 
+# x = provide_input(player="input-provider")
+# y = provide_input(player="model-provider")
+# assert x.dispatch_id == "private"
+# assert y.dispatch_id == "private"
+
+# Temporary:
 x = tf.contant([1], dtype=tf.int64)
 y = tf.constant([2], dtype=tf.int64)
 x.dispatch_id = "private"
