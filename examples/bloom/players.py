d3ef6092db59621ab4c7c1e24dcc4b0548ab2c8f
jvmancuso <jason@dropoutlabs.com>
2019-07-09 23:35:59 +0200

    stashing

---
+++ examples/bloom/players.py
@@ -0,0 +1,84 @@
+"""Code for each data owner in the collaboration."""
+import tensorflow as tf
+
+
+class DataOwner:
+  """Contains code meant to be executed by a data owner Player."""
+  def __init__(
+      self,
+      player_name,
+      data_partition,
+      batch_size,
+  ):
+    self.player_name = player_name
+    self.num_players = num_players
+    self.batch_size = batch_size
+    self.train_initializer = None
+    self.test_initializer = None
+
+  @property
+  def initializer(self):
+    return tf.group(self.train_initializer, self.test_initializer)
+
+  def provide_training_data(self):
+    """Preprocess training dataset
+
+    Return single batch of training dataset
+    """
+    def norm(x, y):
+      return tf.cast(x, tf.float32), tf.expand_dims(y, 0)
+
+    x_raw = tf.random.uniform(
+        minval=-.5,
+        maxval=.5,
+        shape=[self.training_set_size, self.num_features])
+
+    y_raw = tf.cast(tf.reduce_mean(x_raw, axis=1) > 0, dtype=tf.float32)
+
+    train_set = tf.data.Dataset.from_tensor_slices((x_raw, y_raw)) \
+        .map(norm) \
+        .repeat() \
+        .shuffle(buffer_size=self.batch_size) \
+        .batch(self.batch_size)
+
+    train_set_iterator = train_set.make_initializable_iterator()
+    self.train_initializer = train_set_iterator.initializer
+
+    x, y = train_set_iterator.get_next()
+    x = tf.reshape(x, [self.batch_size, self.num_features])
+    y = tf.reshape(y, [self.batch_size, 1])
+
+    return x, y
+
+  def provide_testing_data(self):
+    """Preprocess testing dataset
+
+    Return single batch of testing dataset
+    """
+    def norm(x, y):
+      return tf.cast(x, tf.float32), tf.expand_dims(y, 0)
+
+    x_raw = tf.random.uniform(
+        minval=-.5,
+        maxval=.5,
+        shape=[self.test_set_size, self.num_features])
+
+    y_raw = tf.cast(tf.reduce_mean(x_raw, axis=1) > 0, dtype=tf.float32)
+
+    test_set = tf.data.Dataset.from_tensor_slices((x_raw, y_raw)) \
+        .map(norm) \
+        .batch(self.test_set_size)
+
+    test_set_iterator = test_set.make_initializable_iterator()
+    self.test_initializer = test_set_iterator.initializer
+
+    x, y = test_set_iterator.get_next()
+    x = tf.reshape(x, [self.test_set_size, self.num_features])
+    y = tf.reshape(y, [self.test_set_size, 1])
+
+    return x, y
+
+
+class Collaboration:
+  def __init__(self, dataset, num_players):
+    pass
