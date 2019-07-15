667a610620460746b806f4ac378e75c039827640
avitalsh <avital.shafran@mail.huji.ac.il>
2019-07-15 15:20:16 +0300

    fixed spaces

--- tf_encrypted/keras/layers/depthwise_conv_test.py
+++ tf_encrypted/keras/layers/depthwise_conv_test.py
@@ -12,55 +12,53 @@ np.random.seed(42)
 
 class TestDepthwiseConv2d(unittest.TestCase):
 
-    def setUp(self):
-        tf.reset_default_graph()
+  def setUp(self):
+    tf.reset_default_graph()
 
-    def test_depthwise_conv2d_bias(self):
-        self._core_depthwise_conv2d(kernel_size=2, use_bias=True)
+  def test_depthwise_conv2d_bias(self):
+    self._core_depthwise_conv2d(kernel_size=2, use_bias=True)
 
-    def test_depthwise_conv2d_nobias(self):
-        self._core_depthwise_conv2d(kernel_size=2, use_bias=False)
+  def test_depthwise_conv2d_nobias(self):
+    self._core_depthwise_conv2d(kernel_size=2, use_bias=False)
 
-    def test_depthwise_conv2d_same_padding(self):
-        self._core_depthwise_conv2d(kernel_size=2, padding='same')
+  def test_depthwise_conv2d_same_padding(self):
+    self._core_depthwise_conv2d(kernel_size=2, padding='same')
 
-    def test_depthwise_conv2d_kernelsize_tuple(self):
-        self._core_depthwise_conv2d(kernel_size=(2, 2))
+  def test_depthwise_conv2d_kernelsize_tuple(self):
+    self._core_depthwise_conv2d(kernel_size=(2, 2))
 
-    def test_depthwise_conv2d_depth_multiplier(self):
-        self._core_depthwise_conv2d(kernel_size=2, depth_multiplier=2)
+  def test_depthwise_conv2d_depth_multiplier(self):
+    self._core_depthwise_conv2d(kernel_size=2, depth_multiplier=2)
 
-    def _core_depthwise_conv2d(self, **layer_kwargs):
-        filters_in = 3
-        input_shape = [2, 6, 6, filters_in]  # channels last
+  def _core_depthwise_conv2d(self, **layer_kwargs):
+    filters_in = 3
+    input_shape = [2, 6, 6, filters_in]  # channels last
 
-        if isinstance(layer_kwargs['kernel_size'], int):
-            kernel_size_in = (layer_kwargs['kernel_size'],) * 2
-        else:
-            kernel_size_in = layer_kwargs['kernel_size']
+    if isinstance(layer_kwargs['kernel_size'], int):
+      kernel_size_in = (layer_kwargs['kernel_size'],) * 2
+    else:
+      kernel_size_in = layer_kwargs['kernel_size']
 
-        if 'depth_multiplier' in layer_kwargs:
-            filters_out = layer_kwargs['depth_multiplier']
-        else:
-            filters_out = 1
-        kernel = np.random.normal(kernel_size_in +
-                                  (filters_in, filters_out))
+    filters_out = layer_kwargs.get('depth_multiplier', 1)
 
-        initializer = tf.keras.initializers.Constant(kernel)
+    kernel = np.random.normal(kernel_size_in +
+                              (filters_in, filters_out))
 
-        base_kwargs = {
-            "strides": 2,
-            "depthwise_initializer": initializer,
-        }
+    initializer = tf.keras.initializers.Constant(kernel)
 
-        kwargs = {**base_kwargs, **layer_kwargs}
-        agreement_test(tfe.keras.layers.DepthwiseConv2D,
-                       kwargs=kwargs,
-                       input_shape=input_shape)
-        layer_test(tfe.keras.layers.DepthwiseConv2D,
+    base_kwargs = {
+      "strides": 2,
+      "depthwise_initializer": initializer,
+    }
+
+    kwargs = {**base_kwargs, **layer_kwargs}
+    agreement_test(tfe.keras.layers.DepthwiseConv2D,
                    kwargs=kwargs,
-                   batch_input_shape=input_shape)
+                   input_shape=input_shape)
+    layer_test(tfe.keras.layers.DepthwiseConv2D,
+               kwargs=kwargs,
+               batch_input_shape=input_shape)
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
