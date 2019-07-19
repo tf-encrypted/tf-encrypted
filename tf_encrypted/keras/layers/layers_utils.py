bdb4f101d02f133ba2b1acc095439a4718dc0096
Morten Dahl <mortendahlcs@gmail.com>
2019-07-19 13:00:35 +0200

    more detailed error message

--- tf_encrypted/keras/layers/layers_utils.py
+++ tf_encrypted/keras/layers/layers_utils.py
@@ -4,8 +4,34 @@ import inspect
 import tensorflow as tf
 
 
+class UnknownLayerArgError(ValueError):
+  """Raise error for unknown layer arguments.
+
+    Args:
+      arg_name: TF Encrypted Keras layer argument name (string)
+      layer_sign: TensorFlow Keras layer signature (dict)
+      tf_layer_name: TensorFlow Keras layer name (string)
+  """
+  def __init__(self, arg_name, layer_sign, layer_name):
+    super(UnknownLayerArgError, self).__init__()
+    self.arg_name = arg_name
+    self.layer_sign = layer_sign
+    self.layer_name = layer_name
+
+  def __str__(self):
+    msg = (
+        "Argument '{arg_name}' is not part of the "
+        "signature for '{layer_name}' layers: {layer_sign}"
+    )
+    return msg.format(
+        arg_name=self.arg_name,
+        layer_name=self.layer_name,
+        layer_sign=self.layer_sign.keys(),
+    )
+
+
 class LayerArgNotImplementedError(NotImplementedError):
-  """Raise error when layer argument is not yet supported in TFE
+  """Raise error when layer argument is not yet supported in TFE.
 
     Args:
       arg: TFE layer argument
@@ -39,6 +65,8 @@ def default_args_check(arg, arg_name, tf_layer_name):
   """
   tf_layer_cls = getattr(tf.keras.layers, tf_layer_name)
   layer_sign = inspect.signature(tf_layer_cls.__init__).parameters
+  if arg_name not in layer_sign:
+    raise UnknownLayerArgError(arg_name, layer_sign, tf_layer_name)
   tf_default_arg = layer_sign[arg_name].default
   if arg != tf_default_arg:
     raise LayerArgNotImplementedError(arg_name, tf_layer_name, tf_default_arg)
