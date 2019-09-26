9c93e4b2ed53ec1c820706f9b478a31cbdee1016
Justin Patriquin <justinpatriquin@gmail.com>
2019-09-26 11:20:13 -0300

    leave 1.12.0

--- setup.py
+++ setup.py
@@ -14,12 +14,12 @@ setuptools.setup(
     ]},
     python_requires=">=3.5",
     install_requires=[
-        "tensorflow >=1.13.1, <2",
+        "tensorflow >=1.12.0, <2",
         "numpy >=1.14.0",
         "pyyaml >=5.1",
     ],
     extras_require={
-        "tf": ["tensorflow>=1.13.1,<2"],
+        "tf": ["tensorflow>=1.12.0,<2"],
     },
     license="Apache License 2.0",
     url="https://github.com/tf-encrypted/tf-encrypted",
