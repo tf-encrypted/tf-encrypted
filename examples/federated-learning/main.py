8a7b5ad10216e7ba0b12760c4c7c58d6d105aa99
jvmancuso <jason@dropoutlabs.com>
2019-11-04 12:34:06 -0500

    clean up main script for simulation functionality

--- examples/federated-learning/main.py
+++ examples/federated-learning/main.py
@@ -11,6 +11,7 @@ from absl import flags
 import tensorflow as tf
 import tf_encrypted as tfe
 
+from data_lib import federate_dataset, from_simulated_dataset
 from players import BaseModelOwner, BaseDataOwner
 from func_lib import (
     default_model_fn,
@@ -18,22 +19,31 @@ from func_lib import (
     evaluate_classifier,
     secure_reptile,
     reptile_model_fn,
-    default_build_data_pipeline
+    mnist_data_pipeline
 )
 
 # Data flags
 flags.DEFINE_string("remote_config", None, "Specify remote configuration")
-flags.DEFINE_string("data_root", "./data",
-                    "Specify the root directory of the data")
-flags.DEFINE_integer("data_items", 60000, "Length of dataset")
+flags.DEFINE_list("local_tfrecords", None,
+                  ("Specify list of paths to TFRecord files on data owners "
+                   "All paths supplied must exist on all data owners and "
+                   "point to valid and identically processed TFRecord files. "
+                   "If None, simulation will be assumed."))
+flags.DEFINE_bool("validation_tfrecords", None,
+                  ("Path to TFRecords on model owner to use for validation "
+                   "data. If None, skip validation."))
+flags.DEFINE_integer("evaluate_every", 20,
+                     ("Evaluation frequency: the model owner will run an "
+                      "evaluation step after this many rounds. If None, no "
+                      "validation will happen."))
 flags.DEFINE_integer("num_data_owners", 3,
                      ("Specify how many data owners should "
                       "take part in the learning. If remote "
                       "configuration is specified the number of data owners "
-                      "must match the number specified here"))
+                      "there must match the number specified here."))
 
 # Learning flags
-flags.DEFINE_float("learning_rate", .01, "Global learning rate")
+flags.DEFINE_float("learning_rate", .01, "Global learning rate.")
 flags.DEFINE_float("local_learning_rate", None,
                    ("Local learning rate (only used for reptile learning "
                     "rule). If None, defaults to `learning_rate`."))
@@ -41,17 +51,26 @@ flags.DEFINE_boolean("reptile", False,
                      ("If True, the ModelOwner will use the Reptile "
                       "meta-learning algorithm for performing updates to the "
                       "master model."))
-flags.DEFINE_integer("epochs", 3, ("Number of epochs - used with number of "
-                                   "data owners & batch size to determine "
-                                   "total number of iterations"))
-flags.DEFINE_integer("batch_size", 100, "Batch size")
+flags.DEFINE_integer("rounds", 1000,
+                     ("Number of rounds of training. A single round is "
+                      "defined by one mini-batch update for each data owner, "
+                      "plus an aggregation step by the model owner. A good "
+                      "default would be: desired epochs * len(dataset) // "
+                      "num_data_owners // batch_size."))
+flags.DEFINE_integer("batch_size", 100, "Local batch size")
 
-# Misc flags
-flags.DEFINE_boolean("split", True,
-                     ("Whether the script runner should help "
-                      "simulate the training by splitting the data and "
-                      "distributing it amongst the data owners. "
-                      "Only applicable for local computations."))
+# Simulation flags
+flags.DEFINE_boolean("simulation", True,
+                     ("Whether the script runner should help simulate the "
+                     "training by splitting the data and distributing it "
+                     "among the participants."))
+flags.DEFINE_string("dataset", "fashion_mnist:3.0.0",
+                    ("Specify a TF Dataset to perform supervised learning on. "
+                     "Must be a TF Dataset compatible with the S3 API. For "
+                     "simulation only."))
+flags.DEFINE_float("validation_split", .2,
+                   ("Proportion of training set to hold out for validation on "
+                    "model owner. For simulation only."))
 flags.DEFINE_string("save_path", None,
                     ("Where the model should be saved. If None the "
                      "model isn't saved. With save_format 'h5' "
@@ -62,27 +81,6 @@ flags.DEFINE_string("save_format", "h5",
                      "'tf' saves it as a SavedModel."))
 
 FLAGS = flags.FLAGS
-
-def split_dataset(num_data_owners):
-  """
-  Helper function to help split the dataset evenly between
-  data owners. USE FOR SIMULATION ONLY.
-  """
-
-  print("WARNING: Splitting dataset for {} data owners. "
-        "This is for simulation use only".format(num_data_owners))
-
-  all_dataset = tf.data.TFRecordDataset([FLAGS.data_root + "/train.tfrecord"])
-
-  split = FLAGS.data_items // num_data_owners
-  index = 0
-  for i in range(num_data_owners):
-    dataset = all_dataset.skip(index)
-    dataset = all_dataset.take(split)
-
-    filename = '{}/train{}.tfrecord'.format(FLAGS.data_root, i)
-    writer = tf.data.experimental.TFRecordWriter(filename)
-    writer.write(dataset)
 
 ### Owner classes ###
 class ModelOwner(BaseModelOwner):
@@ -110,9 +108,9 @@ class ModelOwner(BaseModelOwner):
   def evaluator_fn(cls, model_owner):
     return evaluate_classifier(model_owner)
 
-  @classmethod
-  def build_data_pipeline(cls, local_data_file):
-    return default_build_data_pipeline(local_data_file, 50)
+  def build_data_pipeline(self, dataset):
+    return mnist_data_pipeline(dataset, 50)
+
 
 class DataOwner(BaseDataOwner):
   """Contains methods meant to be executed by a data owner.
@@ -123,31 +121,50 @@ class DataOwner(BaseDataOwner):
     build_update_step: `Callable`, the function used to construct
                        a local federated learning update.
   """
-  @classmethod
-  def build_data_pipeline(cls, local_data_file):
-    return default_build_data_pipeline(local_data_file, FLAGS.batch_size)
 
-  # TODO: can move model_fn in here -- we leave it up to the user atm
+  def build_data_pipeline(self, dataset):
+    return mnist_data_pipeline(dataset, FLAGS.batch_size)
+
 
 def main(_):
+  data_owners = [
+      'data-owner-{}'.format(i) for i in range(FLAGS.num_data_owners)
+  ]
+
   if FLAGS.remote_config is not None:
     config = tfe.RemoteConfig.load(FLAGS.remote_config)
     config.connect_to_cluster()
   else:
     players = ['server0', 'server1', 'crypto-producer', 'model-owner']
-    data_owners = [
-        'data-owner-{}'.format(i) for i in range(FLAGS.num_data_owners)
-    ]
     config = tfe.EagerLocalConfig(players + data_owners)
 
   tfe.set_config(config)
   tfe.set_protocol(tfe.protocol.Pond())
 
+  if FLAGS.simulation:
+    if FLAGS.validation_split is None:
+      model_owner = None
+    else:
+      model_owner = 'model-owner'
 
-  batches = FLAGS.data_items // FLAGS.num_data_owners // FLAGS.batch_size
+    DataOwner.build_data_pipeline = from_simulated_dataset(DataOwner.build_data_pipeline)
+    ModelOwner.build_data_pipeline = from_simulated_dataset(ModelOwner.build_data_pipeline)
 
-  if FLAGS.split:
-    split_dataset(FLAGS.num_data_owners)
+    simulation_tfrecords = federate_dataset(
+        FLAGS.dataset,
+        data_owners,
+        model_owner,
+        validation_split=FLAGS.validation_split,
+    )
+
+    dataowner_tfrecords = {do: simulation_tfrecords[do] for do in data_owners}
+    validation_tfrecords = simulation_tfrecords.get('model-owner', None)
+  else:
+    simulation_tfrecords = None
+    dataowner_tfrecords = FLAGS.local_tfrecords
+    validation_tfrecords = FLAGS.validation_tfrecords
+
+
 
   logging.basicConfig(level=logging.DEBUG)
 
@@ -164,19 +181,19 @@ def main(_):
   local_lr = FLAGS.local_learning_rate or FLAGS.learning_rate
 
   model_owner = ModelOwner(
+      # TODO: generalize this name?
       "model-owner",
-      "{}/train.tfrecord".format(FLAGS.data_root),
+      validation_tfrecords,
       model, loss,
       optimizer=tf.keras.optimizers.Adam(FLAGS.learning_rate)
   )
 
-  data_owners = [DataOwner("data-owner-{}".format(i),
-                           "{}/train{}.tfrecord".format(FLAGS.data_root, i),
-                           model, loss,
+  data_owners = [DataOwner(data_owner,
+                           dataowner_tfrecords[data_owner], model, loss,
                            optimizer=tf.keras.optimizers.Adam(local_lr))
-                 for i in range(FLAGS.num_data_owners)]
+                 for data_owner in dataowner_tfrecords]
 
-  model_owner.fit(data_owners, rounds=batches, evaluate_every=10)
+  model_owner.fit(data_owners, rounds=FLAGS.rounds, evaluate_every=10)
 
   if FLAGS.save_path:
     model_owner.save_model(FLAGS.save_path, FLAGS.save_format)
