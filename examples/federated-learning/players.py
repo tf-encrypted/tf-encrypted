Bugfix: FL example broadcast leak (#719)
* bugfix: stay in-graph during broadcast phase

* bugfix extension: pin to current device

* 0.4.8

examples/federated-learning/players.py
@@ -80,9 +80,12 @@ class BaseModelOwner:
       self._update_one_round(data_owners, **kwargs)
 
       # Broadcast master model
+      # TODO: don't assume it's a tf.keras model
+      master_vars = self.model.trainable_variables
       for owner in data_owners:
-        # TODO: don't assume it's a tf.keras model
-        owner.model.set_weights(self.model.get_weights())
+        with owner.device:
+          for v_d, v_m in zip(owner.model.trainable_variables, master_vars):
+            v_d.assign(v_m)
 
       # Evaluate once (maybe)
       if evaluate and (r + 1) % evaluate_every == 0: