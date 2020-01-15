9f9dbbe08c5ebabe9d7579b574cc7f392ea899e9
Morten Dahl <mortendahlcs@gmail.com>
2020-01-15 14:50:27 +0100

    ..

--- tf_encrypted/tensor/boolfactory.py
+++ tf_encrypted/tensor/boolfactory.py
@@ -8,138 +8,15 @@ import abc
 import numpy as np
 import tensorflow as tf
 
-from .factory import (AbstractFactory, AbstractTensor, AbstractVariable,
-                      AbstractConstant, AbstractPlaceholder)
+from .factory import AbstractFactory
+from .factory import AbstractTensor
+from .factory import AbstractVariable
+from .factory import AbstractConstant
+from .factory import AbstractPlaceholder
 from ..operations import secure_random as crypto
 
 
 def bool_factory():
-    """Constructs the native tensor Factory."""
-
-    class Factory(AbstractFactory):
-        """Native tensor factory."""
-
-        def tensor(self, value):
-            if isinstance(value, Tensor):
-                return value
-
-            if isinstance(value, tf.Tensor):
-                if value.dtype is not self.native_type:
-                    value = tf.cast(value, dtype=self.native_type)
-                return DenseTensor(value)
-
-            value = np.array(value)
-            value = tf.convert_to_tensor(value)
-            value = tf.cast(value, self.native_type)
-            return DenseTensor(value)
-
-        def constant(self, value):
-            value = tf.constant(value, dtype=self.native_type)
-            return Constant(value)
-
-        def variable(self, initial_value):
-            if isinstance(initial_value, (tf.Tensor, np.ndarray)):
-                return Variable(initial_value)
-
-            if isinstance(initial_value, Tensor):
-                return Variable(initial_value.value)
-
-            msg = "Don't know how to handle {}"
-            raise TypeError(msg.format(type(initial_value)))
-
-        def placeholder(self, shape):
-            return Placeholder(shape)
-
-        @property
-        def native_type(self):
-            return tf.bool
-
-        def modulus(self) -> int:
-            return 2
-
-        def sample_uniform(self, shape):
-            minval = 0
-            maxval = 2
-
-            if crypto.supports_seeded_randomness():
-                seed = crypto.secure_seed()
-                return UniformTensor(shape=shape,
-                                     seed=seed,
-                                     minval=minval,
-                                     maxval=maxval)
-
-            if crypto.supports_secure_randomness():
-                sampler = crypto.random_uniform
-            else:
-                sampler = tf.random_uniform
-            value = sampler(
-                shape=shape,
-                minval=minval,
-                maxval=maxval,
-                dtype=tf.int32
-            )
-            value = tf.cast(value, tf.bool)
-            return DenseTensor(value)
-
-        def sample_seeded_uniform(self,
-                                  shape,
-                                  seed):
-            minval = 0
-            maxval = 2
-
-            if crypto.supports_seeded_randomness():
-                # Don't use UniformTensor for lazy sampling here, because the `seed` might be something (e.g., key) we
-                # want to protect, and we cannot send it to another party
-                value = crypto.seeded_random_uniform(
-                    shape=shape,
-                    dtype=tf.int32,
-                    minval=minval,
-                    maxval=maxval,
-                    seed=seed,
-                )
-                value = tf.cast(value, tf.bool)
-                return DenseTensor(value)
-            else:
-                raise NotImplementedError("Secure seeded randomness implementation is not available.")
-
-        def sample_bounded(self,
-                           shape,
-                           bitlength: int):
-            raise NotImplementedError("No bounded sampling for boolean type.")
-
-        def stack(self, xs: list, axis: int = 0):
-            assert all(isinstance(x, Tensor) for x in xs)
-            value = tf.stack([x.value for x in xs], axis=axis)
-            return DenseTensor(value)
-
-        def concat(self, xs: list, axis: int):
-            assert all(isinstance(x, Tensor) for x in xs)
-            value = tf.concat([x.value for x in xs], axis=axis)
-            return DenseTensor(value)
-
-        def where(self, condition, x, y, v2=True):
-            if not isinstance(condition, tf.Tensor):
-                msg = "Don't know how to handle `condition` of type {}"
-                raise TypeError(msg.format(type(condition)))
-            if not v2:
-                value = tf.where(condition, x.value, y.value)
-            else:
-                value = tf.where_v2(condition, x.value, y.value)
-            return DenseTensor(value)
-
-    def _lift(x, y) -> Tuple['Tensor', 'Tensor']:
-
-        if isinstance(x, Tensor) and isinstance(y, Tensor):
-            return x, y
-
-        if isinstance(x, Tensor):
-            return x, x.factory.tensor(y)
-
-        if isinstance(y, Tensor):
-            return y.factory.tensor(x), y
-
-        raise TypeError("Don't know how to lift {} {}".format(type(x), type(y)))
-
     class Tensor(AbstractTensor):
         """Base class for other native tensor classes."""
 
@@ -186,6 +63,121 @@ def bool_factory():
         def split(self, num_split: int, axis: int = 0):
             values = tf.split(self.value, num_split, axis=axis)
             return [DenseTensor(value) for value in values]
+  """Constructs the native tensor Factory."""
+
+  class Factory(AbstractFactory):
+    """Native tensor factory."""
+
+    def tensor(self, value):
+      if isinstance(value, Tensor):
+        return value
+
+      if isinstance(value, tf.Tensor):
+        if value.dtype is not self.native_type:
+          value = tf.cast(value, dtype=self.native_type)
+        return DenseTensor(value)
+
+      value = np.array(value)
+      value = tf.convert_to_tensor(value)
+      value = tf.cast(value, self.native_type)
+      return DenseTensor(value)
+
+    def constant(self, value):
+      value = tf.constant(value, dtype=self.native_type)
+      return Constant(value)
+
+    def variable(self, initial_value):
+      if isinstance(initial_value, (tf.Tensor, np.ndarray)):
+        return Variable(initial_value)
+
+      if isinstance(initial_value, Tensor):
+        return Variable(initial_value.value)
+
+      msg = "Don't know how to handle {}"
+      raise TypeError(msg.format(type(initial_value)))
+
+    def placeholder(self, shape):
+      return Placeholder(shape)
+
+    @property
+    def native_type(self):
+      return tf.bool
+
+    def modulus(self) -> int:
+      return 2
+
+    def sample_uniform(self, shape):
+      minval = 0
+      maxval = 2
+
+      if crypto.supports_seeded_randomness():
+        seed = crypto.secure_seed()
+        return UniformTensor(shape=shape, seed=seed, minval=minval, maxval=maxval)
+
+      if crypto.supports_secure_randomness():
+        sampler = crypto.random_uniform
+      else:
+        sampler = tf.random_uniform
+      value = sampler(shape=shape, minval=minval, maxval=maxval, dtype=tf.int32)
+      value = tf.cast(value, tf.bool)
+      return DenseTensor(value)
+
+    def sample_seeded_uniform(self, shape, seed):
+      minval = 0
+      maxval = 2
+
+      if crypto.supports_seeded_randomness():
+        # Don't use UniformTensor for lazy sampling here, because the `seed` might be something (e.g., key) we
+        # want to protect, and we cannot send it to another party
+        value = crypto.seeded_random_uniform(
+            shape=shape,
+            dtype=tf.int32,
+            minval=minval,
+            maxval=maxval,
+            seed=seed,
+        )
+        value = tf.cast(value, tf.bool)
+        return DenseTensor(value)
+      else:
+        raise NotImplementedError(
+            "Secure seeded randomness implementation is not available.")
+
+    def sample_bounded(self, shape, bitlength: int):
+      raise NotImplementedError("No bounded sampling for boolean type.")
+
+    def stack(self, xs: list, axis: int = 0):
+      assert all(isinstance(x, Tensor) for x in xs)
+      value = tf.stack([x.value for x in xs], axis=axis)
+      return DenseTensor(value)
+
+    def concat(self, xs: list, axis: int):
+      assert all(isinstance(x, Tensor) for x in xs)
+      value = tf.concat([x.value for x in xs], axis=axis)
+      return DenseTensor(value)
+
+    def where(self, condition, x, y, v2=True):
+      if not isinstance(condition, tf.Tensor):
+        msg = "Don't know how to handle `condition` of type {}"
+        raise TypeError(msg.format(type(condition)))
+      if not v2:
+        value = tf.where(condition, x.value, y.value)
+      else:
+        value = tf.where_v2(condition, x.value, y.value)
+      return DenseTensor(value)
+
+  def _lift(x, y) -> Tuple['Tensor', 'Tensor']:
+
+    if isinstance(x, Tensor) and isinstance(y, Tensor):
+      return x, y
+
+    if isinstance(x, Tensor):
+      return x, x.factory.tensor(y)
+
+    if isinstance(y, Tensor):
+      return y.factory.tensor(x), y
+
+    raise TypeError("Don't know how to lift {} {}".format(type(x), type(y)))
+
 
         def reshape(self, axes: Union[tf.Tensor, List[int]]):
             return DenseTensor(tf.reshape(self.value, axes))
