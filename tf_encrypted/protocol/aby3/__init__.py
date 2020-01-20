ec437049b1b7a46f713fc1ebf943f23223b05679
Morten Dahl <mortendahlcs@gmail.com>
2020-01-20 10:37:15 +0100

    ABY3 formatting (#735)

--- tf_encrypted/protocol/aby3/__init__.py
+++ tf_encrypted/protocol/aby3/__init__.py
@@ -1,5 +1,6 @@
-from .aby3 import ABY3, ABY3Tensor, ABY3PublicTensor, ABY3PrivateTensor, ARITHMETIC, BOOLEAN
-
+from .aby3 import ABY3
+from .aby3 import ABY3Tensor, ABY3PublicTensor, ABY3PrivateTensor
+from .aby3 import ARITHMETIC, BOOLEAN
 
 __all__ = [
     "ABY3",
