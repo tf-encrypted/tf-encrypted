9499a303ecd93e5ae41d9212c67ff410f34076d4
Morten Dahl <mortendahlcs@gmail.com>
2020-02-25 12:15:01 +0100

    make sure README.md is parsed as UTF8 in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """Installing with setuptools."""
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf8") as fh:
   long_description = fh.read()
 
 setuptools.setup(
