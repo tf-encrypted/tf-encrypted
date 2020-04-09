52b999972a2e6d416f50080acdc17e931a83fc72
Morten Dahl <mortendahlcs@gmail.com>
2020-04-09 23:17:52 +0200

    shape inference functions for select ops (#788)

--- primitives/tf_encrypted/primitives/sodium/cc/ops.cc
+++ primitives/tf_encrypted/primitives/sodium/cc/ops.cc
@@ -1,6 +1,11 @@
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/common_shape_fns.h"
 #include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/framework/types.h"
+
+#include "sodium.h"
+
+using namespace tensorflow;
 
 //
 // Easy Box ops
@@ -9,11 +14,25 @@
 REGISTER_OP("SodiumEasyBoxGenKeypair")
     .Output("pk: uint8")
     .Output("sk: uint8")
-    .SetIsStateful();
+    .SetIsStateful()
+    .SetShapeFn([](shape_inference::InferenceContext* context){
+        shape_inference::ShapeHandle pk_shape = context->MakeShape({crypto_box_PUBLICKEYBYTES});
+        context->set_output(0, pk_shape);
+
+        shape_inference::ShapeHandle sk_shape = context->MakeShape({crypto_box_SECRETKEYBYTES});
+        context->set_output(1, sk_shape);
+
+        return Status::OK();
+    });;
 
 REGISTER_OP("SodiumEasyBoxGenNonce")
     .Output("nonce: uint8")
-    .SetIsStateful();
+    .SetIsStateful()
+    .SetShapeFn([](shape_inference::InferenceContext* context){
+        shape_inference::ShapeHandle nonce_shape = context->MakeShape({crypto_box_NONCEBYTES});
+        context->set_output(0, nonce_shape);
+        return Status::OK();
+    });
 
 REGISTER_OP("SodiumEasyBoxSealDetached")
     .Attr("plaintext_dtype: {uint8, float32}")
@@ -23,7 +42,41 @@ REGISTER_OP("SodiumEasyBoxSealDetached")
     .Input("sk_sender: uint8")
     .Output("ciphertext: uint8")
     .Output("mac: uint8")
-    .SetIsStateful();
+    .SetIsStateful()
+    .SetShapeFn([](shape_inference::InferenceContext* c){
+
+        //
+        // Check input shapes; also done in kernel
+        //
+        
+        if (!c->MergeInput(1, c->MakeShape({crypto_box_NONCEBYTES}))){
+            return errors::Internal("Nonce not of the required shape");
+        }
+        if (!c->MergeInput(2, c->MakeShape({crypto_box_PUBLICKEYBYTES}))){
+            return errors::Internal("Public key not of the required shape");
+        }
+        if (!c->MergeInput(3, c->MakeShape({crypto_box_SECRETKEYBYTES}))){
+            return errors::Internal("Secret key not of the required shape");
+        }
+
+        //
+        // Compute output shapes
+        //
+
+        tensorflow::DataType plaintext_dtype;
+        TF_RETURN_IF_ERROR(c->GetAttr("plaintext_dtype", &plaintext_dtype));
+        int ciphertext_expansion = tensorflow::DataTypeSize(plaintext_dtype);
+
+        shape_inference::ShapeHandle plaintext_shape = c->input(0);
+        shape_inference::ShapeHandle ciphertext_shape;
+        TF_RETURN_IF_ERROR(c->Concatenate(plaintext_shape, c->MakeShape({ciphertext_expansion}), &ciphertext_shape));
+        c->set_output(0, ciphertext_shape);
+
+        shape_inference::ShapeHandle mac_shape = c->MakeShape({crypto_box_MACBYTES});
+        c->set_output(1, mac_shape);
+
+        return Status::OK();
+    });
 
 REGISTER_OP("SodiumEasyBoxOpenDetached")
     .Attr("plaintext_dtype: {uint8, float32}")
@@ -33,4 +86,44 @@ REGISTER_OP("SodiumEasyBoxOpenDetached")
     .Input("pk_sender: uint8")
     .Input("sk_receiver: uint8")
     .Output("plaintext: plaintext_dtype")
-    .SetIsStateful();
+    .SetIsStateful()
+    .SetShapeFn([](shape_inference::InferenceContext* c){
+
+        //
+        // Check input shapes; also done in kernel
+        //
+
+        tensorflow::DataType plaintext_dtype;
+        TF_RETURN_IF_ERROR(c->GetAttr("plaintext_dtype", &plaintext_dtype));
+        int ciphertext_expansion = tensorflow::DataTypeSize(plaintext_dtype);
+
+        shape_inference::ShapeHandle ciphertext_shape = c->input(0);
+        TF_RETURN_IF_ERROR(c->WithRankAtLeast(ciphertext_shape, 1, &ciphertext_shape));
+        int64 last_dim = c->Value(c->Dim(ciphertext_shape, -1));
+        if (last_dim != ciphertext_expansion) {
+            return errors::Internal("Last dimension of ciphertext does not match plaintext_dtype ", last_dim);
+        }
+
+        if (!c->MergeInput(1, c->MakeShape({crypto_box_MACBYTES}))){
+            return errors::Internal("Mac not of the required shape");
+        }
+        if (!c->MergeInput(2, c->MakeShape({crypto_box_NONCEBYTES}))){
+            return errors::Internal("Nonce not of the required shape");
+        }
+        if (!c->MergeInput(3, c->MakeShape({crypto_box_PUBLICKEYBYTES}))){
+            return errors::Internal("Public key not of the required shape");
+        }
+        if (!c->MergeInput(4, c->MakeShape({crypto_box_SECRETKEYBYTES}))){
+            return errors::Internal("Secret key not of the required shape");
+        }
+
+        //
+        // Compute output shape
+        //
+
+        shape_inference::ShapeHandle plaintext_shape;
+        TF_RETURN_IF_ERROR(c->Subshape(ciphertext_shape, 0, -1, &plaintext_shape));
+        c->set_output(0, plaintext_shape);
+
+        return Status::OK();
+    });
--- primitives/tf_encrypted/primitives/sodium/python/easy_box_test.py
+++ primitives/tf_encrypted/primitives/sodium/python/easy_box_test.py
@@ -1,5 +1,7 @@
 # pylint: disable=missing-docstring
 import unittest
+from absl.testing import parameterized
+import contextlib
 
 import numpy as np
 import tensorflow as tf
@@ -7,68 +9,80 @@ import tensorflow as tf
 from tf_encrypted.primitives.sodium.python import easy_box
 
 
-class TestEasyBox(unittest.TestCase):
-    def test_gen_keypair(self):
-        pk, sk = easy_box.gen_keypair()
-        assert isinstance(pk, easy_box.PublicKey), type(pk)
-        assert isinstance(sk, easy_box.SecretKey), type(sk)
+def tf_execution_mode(eager):
+    if not eager:
+        return tf.Graph().as_default()
+    return contextlib.suppress()
 
-    def test_gen_nonce(self):
-        nonce = easy_box.gen_nonce()
+
+class TestExecutionMode(parameterized.TestCase):
+    @parameterized.parameters(True, False)
+    def test_tf_execution_mode(self, eager: bool):
+        with tf_execution_mode(eager):
+            assert tf.executing_eagerly() == eager
+
+
+class TestEasyBox(parameterized.TestCase):
+    @parameterized.parameters(
+        {"eager": True}, {"eager": False},
+    )
+    def test_gen_keypair(self, eager):
+        with tf_execution_mode(eager):
+            pk, sk = easy_box.gen_keypair()
+
+        assert isinstance(pk, easy_box.PublicKey), type(pk)
+        assert pk.raw.dtype == tf.uint8
+        assert pk.raw.shape == (32,)
+
+        assert isinstance(sk, easy_box.SecretKey), type(sk)
+        assert sk.raw.dtype == tf.uint8
+        assert sk.raw.shape == (32,)
+
+    @parameterized.parameters(True, False)
+    def test_gen_nonce(self, eager):
+        with tf_execution_mode(eager):
+            nonce = easy_box.gen_nonce()
+
+        assert nonce.raw.dtype == tf.uint8
+        assert nonce.raw.shape == (24,)
         assert isinstance(nonce, easy_box.Nonce), type(nonce)
         assert isinstance(nonce.raw, tf.Tensor)
 
-    def test_seal_uint8(self):
-        _, sk_s = easy_box.gen_keypair()
-        pk_r, _ = easy_box.gen_keypair()
+    @parameterized.parameters(
+        {"eager": eager, "m": m, "dtype": dtype, "dtype_size": dtype_size}
+        for eager in (True, False)
+        for m in (5, [5], [[1, 2], [3, 4]])
+        for dtype, dtype_size in [(tf.uint8, 1), (tf.float32, 4)]
+    )
+    def test_seal_and_open(self, eager, m, dtype, dtype_size):
+        with tf_execution_mode(eager):
+            pk_s, sk_s = easy_box.gen_keypair()
+            pk_r, sk_r = easy_box.gen_keypair()
 
-        plaintext = tf.constant([1, 2, 3, 4], shape=(2, 2), dtype=tf.uint8)
+            plaintext = tf.constant(m, dtype=dtype)
 
-        nonce = easy_box.gen_nonce()
-        ciphertext, _ = easy_box.seal_detached(plaintext, nonce, pk_r, sk_s)
+            nonce = easy_box.gen_nonce()
+            ciphertext, mac = easy_box.seal_detached(plaintext, nonce, pk_r, sk_s)
 
-        assert ciphertext.raw.shape == plaintext.shape + (1,)
+            plaintext_recovered = easy_box.open_detached(
+                ciphertext, mac, nonce, pk_s, sk_r, plaintext.dtype
+            )
 
-    def test_seal_floats(self):
-        _, sk_s = easy_box.gen_keypair()
-        pk_r, _ = easy_box.gen_keypair()
+        assert isinstance(ciphertext, easy_box.Ciphertext)
+        assert isinstance(ciphertext.raw, tf.Tensor)
+        assert ciphertext.raw.dtype == tf.uint8
+        assert ciphertext.raw.shape == plaintext.shape + (dtype_size,)
 
-        plaintext = tf.constant([1, 2, 3, 4], shape=(2, 2), dtype=tf.float32)
+        assert isinstance(mac, easy_box.Mac)
+        assert isinstance(mac.raw, tf.Tensor)
+        assert mac.raw.dtype == tf.uint8
+        assert mac.raw.shape == (16,)
 
-        nonce = easy_box.gen_nonce()
-        ciphertext, _ = easy_box.seal_detached(plaintext, nonce, pk_r, sk_s)
-
-        assert ciphertext.raw.shape == plaintext.shape + (4,)
-
-    def test_open_uint8(self):
-        pk_s, sk_s = easy_box.gen_keypair()
-        pk_r, sk_r = easy_box.gen_keypair()
-
-        plaintext = tf.constant([1, 2, 3, 4], shape=(2, 2), dtype=tf.uint8)
-
-        nonce = easy_box.gen_nonce()
-        ciphertext, mac = easy_box.seal_detached(plaintext, nonce, pk_r, sk_s)
-        plaintext_recovered = easy_box.open_detached(
-            ciphertext, mac, nonce, pk_s, sk_r, plaintext.dtype
-        )
-
+        assert isinstance(plaintext_recovered, tf.Tensor)
+        assert plaintext_recovered.dtype == plaintext.dtype
         assert plaintext_recovered.shape == plaintext.shape
-        np.testing.assert_equal(plaintext_recovered, np.array([[1, 2], [3, 4]]))
-
-    def test_open_float(self):
-        pk_s, sk_s = easy_box.gen_keypair()
-        pk_r, sk_r = easy_box.gen_keypair()
-
-        plaintext = tf.constant([1, 2, 3, 4], shape=(2, 2), dtype=tf.float32)
-
-        nonce = easy_box.gen_nonce()
-        ciphertext, mac = easy_box.seal_detached(plaintext, nonce, pk_r, sk_s)
-        plaintext_recovered = easy_box.open_detached(
-            ciphertext, mac, nonce, pk_s, sk_r, plaintext.dtype
-        )
-
-        assert plaintext_recovered.shape == plaintext.shape
-        np.testing.assert_equal(plaintext_recovered, np.array([[1, 2], [3, 4]]))
+        if eager:
+            np.testing.assert_equal(plaintext_recovered, np.array(m))
 
 
 if __name__ == "__main__":
