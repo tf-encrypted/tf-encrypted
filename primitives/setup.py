1495acda196eee36b672b28cca8b9bd9fb41e940
jvmancuso <jason@dropoutlabs.com>
2020-08-19 12:29:24 -0400

    black

--- primitives/setup.py
+++ primitives/setup.py
@@ -11,10 +11,12 @@ setuptools.setup(
     license="Apache License 2.0",
     url="https://github.com/tf-encrypted/tf-encrypted",
     description="A Framework for Machine Learning on Encrypted Data.",
-    long_description=("Primitives from tf-encrypted. See the `primitives/` "
-                      "folder in the "
-                      "[TFE repo](https://github.com/tf-encrypted/tf-encrypted) "
-                      "for more information."),
+    long_description=(
+        "Primitives from tf-encrypted. See the `primitives/` "
+        "folder in the "
+        "[TFE repo](https://github.com/tf-encrypted/tf-encrypted) "
+        "for more information."
+    ),
     long_description_content_type="text/markdown",
     author="The TF Encrypted Authors",
     author_email="contact@tf-encrypted.io",
