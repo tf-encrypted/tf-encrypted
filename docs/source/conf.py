c0c09af08fea2455c97e6da8d9f71e8124e7393e
zhicong.hzc <zhicong.hzc@alibaba-inc.com>
2022-03-07 12:30:55 +0800

    release candidate: 0.7.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, The TF Encrypted Authors'
 author = 'The TF Encrypted Authors'
 
 # The short X.Y version
-version = '0.5.9'
+version = '0.7.0'
 # The full version, including alpha/beta/rc tags
-release = '0.5.9'
+release = '0.7.0-rc0'
 
 
 # -- General configuration ---------------------------------------------------
