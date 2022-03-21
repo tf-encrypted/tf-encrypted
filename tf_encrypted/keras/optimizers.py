a62192bdb9e5e8a938b9ae47cb2f3c56b79f30b0
zhicong.hzc <zhicong.hzc@alibaba-inc.com>
2022-03-21 15:23:12 +0800

    change lr to learning_rate

--- tf_encrypted/keras/optimizers.py
+++ tf_encrypted/keras/optimizers.py
@@ -7,16 +7,16 @@ class SGD:
     """Stochastic gradient descent optimizer.
 
     Arguments:
-        lr: float >= 0. Learning rate.
+        learning_rate: float >= 0. Learning rate.
     """
 
-    def __init__(self, lr=0.01):
-        self.lr = lr
+    def __init__(self, learning_rate=0.01):
+        self.learning_rate = learning_rate
 
     def apply_gradients(self, var, grad):
         sess = KE.get_session()
         for i, w in enumerate(var):
-            sess.run(tfe.assign(w, w - grad[i] * self.lr))
+            sess.run(tfe.assign(w, w - grad[i] * self.learning_rate))
 
 
 _known_optimizers = {
