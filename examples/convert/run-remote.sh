df016eb6d7abf565e58759eb935a0db2020a66e5
zhicong.hzc <zhicong.hzc@alibaba-inc.com>
2022-05-31 10:41:36 +0800

    add path inference

--- examples/convert/run-remote.sh
+++ examples/convert/run-remote.sh
@@ -1,18 +1,22 @@
+SCRIPT_PATH="${BASH_SOURCE:-$0}"
+ABS_SCRIPT_PATH="$(realpath "${SCRIPT_PATH}")"
+ABS_DIRECTORY="$(dirname "${ABS_SCRIPT_PATH}")"
+
 echo "Starting server0"
-(python -m tf_encrypted.player server0 --config examples/convert/config.json > log0.txt 2>&1 &)
+(python -m tf_encrypted.player server0 --config ${ABS_DIRECTORY}/config.json > log0.txt 2>&1 &)
 sleep 1
 echo "Starting server1"
-(python -m tf_encrypted.player server1 --config examples/convert/config.json > log1.txt 2>&1 &)
+(python -m tf_encrypted.player server1 --config ${ABS_DIRECTORY}/config.json > log1.txt 2>&1 &)
 sleep 1
 echo "Starting server2"
-(python -m tf_encrypted.player server2 --config examples/convert/config.json > log2.txt 2>&1 &)
+(python -m tf_encrypted.player server2 --config ${ABS_DIRECTORY}/config.json > log2.txt 2>&1 &)
 sleep 1
 echo "Starting weights-provider"
-(python -m tf_encrypted.player weights-provider --config examples/convert/config.json > log3.txt 2>&1 &)
+(python -m tf_encrypted.player weights-provider --config ${ABS_DIRECTORY}/config.json > log3.txt 2>&1 &)
 sleep 1
 echo "Starting prediction-client"
-(python -m tf_encrypted.player prediction-client --config examples/convert/config.json > log4.txt 2>&1 &)
+(python -m tf_encrypted.player prediction-client --config ${ABS_DIRECTORY}/config.json > log4.txt 2>&1 &)
 sleep 1
 
 echo "Run private model conversion and prediction example..."
-(time python examples/convert/convert2.py examples/convert/config.json > log.txt 2>&1 &)
+(time python ${ABS_DIRECTORY}/convert2.py ${ABS_DIRECTORY}/config.json > log.txt 2>&1 &)
--- examples/mnist/run-remote.sh
+++ examples/mnist/run-remote.sh
@@ -1,18 +1,22 @@
+SCRIPT_PATH="${BASH_SOURCE:-$0}"
+ABS_SCRIPT_PATH="$(realpath "${SCRIPT_PATH}")"
+ABS_DIRECTORY="$(dirname "${ABS_SCRIPT_PATH}")"
+
 echo "Starting server0"
-(python -m tf_encrypted.player server0 --config examples/mnist/config.json > log0.txt 2>&1 &)
+(python -m tf_encrypted.player server0 --config ${ABS_DIRECTORY}/config.json > log0.txt 2>&1 &)
 sleep 1
 echo "Starting server1"
-(python -m tf_encrypted.player server1 --config examples/mnist/config.json > log1.txt 2>&1 &)
+(python -m tf_encrypted.player server1 --config ${ABS_DIRECTORY}/config.json > log1.txt 2>&1 &)
 sleep 1
 echo "Starting server2"
-(python -m tf_encrypted.player server2 --config examples/mnist/config.json > log2.txt 2>&1 &)
+(python -m tf_encrypted.player server2 --config ${ABS_DIRECTORY}/config.json > log2.txt 2>&1 &)
 sleep 1
 echo "Starting training-client"
-(python -m tf_encrypted.player training-client --config examples/mnist/config.json > log3.txt 2>&1 &)
+(python -m tf_encrypted.player training-client --config ${ABS_DIRECTORY}/config.json > log3.txt 2>&1 &)
 sleep 1
 echo "Starting prediction-client"
-(python -m tf_encrypted.player prediction-client --config examples/mnist/config.json > log4.txt 2>&1 &)
+(python -m tf_encrypted.player prediction-client --config ${ABS_DIRECTORY}/config.json > log4.txt 2>&1 &)
 sleep 1
 
 echo "Run private training example..."
-(time python examples/mnist/private_network_training.py examples/mnist/config.json > log.txt 2>&1 &)
+(time python ${ABS_DIRECTORY}/private_network_training.py ${ABS_DIRECTORY}/config.json > log.txt 2>&1 &)
