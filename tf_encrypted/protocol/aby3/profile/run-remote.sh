87d25dc1fdb3974a24a5af2bfc11ff25c8c143de
zhicong.hzc <zhicong.hzc@alibaba-inc.com>
2022-06-01 16:08:13 +0800

    minor: comment

--- tf_encrypted/protocol/aby3/profile/run-remote.sh
+++ tf_encrypted/protocol/aby3/profile/run-remote.sh
@@ -13,6 +13,6 @@ echo "Starting server2"
 (python -m tf_encrypted.player server2 --config ${ABS_DIRECTORY}/config.json > log2.txt 2>&1 &)
 sleep 1
 
-echo "Run private model conversion and prediction example..."
+echo "Run ABY3 profiling of function: $1"
 (time python ${ABS_DIRECTORY}/aby3_profile.py ${ABS_DIRECTORY}/config.json $1 > log.txt 2>&1 &)
 
