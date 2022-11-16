99a5ca314afcbd0671b8192fe1c2ffdb67a4e6cc
zjn364739 <zjn364739@alibaba-inc.com>
2022-11-16 10:21:34 +0800

    version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+
+## [0.9.0]
+
 **Changed**
 
 - Remove all TF1 like TFE features, e.g. `tfe.Session`, `tfe.Placeholder`...
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, The TF Encrypted Authors'
 author = 'The TF Encrypted Authors'
 
 # The short X.Y version
-version = '0.8.0'
+version = '0.9.0'
 # The full version, including alpha/beta/rc tags
-release = '0.8.0'
+release = '0.9.0'
 
 
 # -- General configuration ---------------------------------------------------
